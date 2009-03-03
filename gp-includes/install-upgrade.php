4e8ff066147335a76c1c484612c0a6503d5be4a3
Nikolay Bachiyski <nb@nikolay.bg>
2009-03-03 14:26:25 +0000

    Move functions out of install.php, they aren't used there

--- gp-includes/install-upgrade.php
+++ gp-includes/install-upgrade.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ * Guesses the final installed URI based on the location of the install script
+ *
+ * @return string The guessed URI
+ */
+function guess_uri()
+{
+	$schema = 'http://';
+	if ( isset( $_SERVER['HTTPS'] ) && strtolower( $_SERVER['HTTPS'] ) == 'on' ) {
+		$schema = 'https://';
+	}
+	$uri = preg_replace( '|/[^/]*$|i', '/', $schema . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
+
+	return rtrim( $uri, " \t\n\r\0\x0B/" ) . '/';
+}
+
+function gp_update_db_version() {
+	gp_update_option( 'gp_db_version', gp_get_option( 'gp_db_version' ) );
+}
+
+
+
 function gp_install() {
 	global $gpdb;
 	
--- install.php
+++ install.php
@@ -9,26 +9,6 @@ require_once( BACKPRESS_PATH . 'class.bp-sql-schema-parser.php' );
 require_once( GP_PATH . GP_INC . 'install-upgrade.php' );
 require_once( GP_PATH . GP_INC . 'schema.php' );
 
-function gp_update_db_version() {
-	gp_update_option( 'gp_db_version', gp_get_option( 'gp_db_version' ) );
-}
-
-/**
- * Guesses the final installed URI based on the location of the install script
- *
- * @return string The guessed URI
- */
-function guess_uri()
-{
-	$schema = 'http://';
-	if ( isset( $_SERVER['HTTPS'] ) && strtolower( $_SERVER['HTTPS'] ) == 'on' ) {
-		$schema = 'https://';
-	}
-	$uri = preg_replace( '|/[^/]*$|i', '/', $schema . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-
-	return rtrim( $uri, " \t\n\r\0\x0B/" ) . '/';
-}
-
 if ( gp_get_option( 'gp_db_version' ) <= gp_get_option_from_db( 'gp_db_version' ) && !isset( $_GET['force'] ) ) {
 	$success_message = __( 'You already have the latest version, no need to upgrade!' );
 	$errors = array();
