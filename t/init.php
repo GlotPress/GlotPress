9679be1eb50c7675d40720f4170f480617294cdc
Nikolay Bachiyski <nb@nikolay.bg>
2009-03-04 13:01:42 +0000

    Do not show notices from installer

--- t/init.php
+++ t/init.php
@@ -8,6 +8,8 @@ require_once '../gp-includes/backpress/class.bp-sql-schema-parser.php';
 require_once '../gp-includes/schema.php';
 require_once '../gp-includes/install-upgrade.php';
 
+error_reporting(E_ALL ^ E_NOTICE);
+
 // TODO: drop all tables, on most hosts users can't drop their databases
 $gpdb->query("DROP DATABASE ".GPDB_NAME.";");
 $gpdb->query("CREATE DATABASE ".GPDB_NAME.";");
