3e68ff71451b5cab72b26f8b10579cf1c617e82b
Nikolay Bachiyski <nb@nikolay.bg>
2009-03-04 12:59:49 +0000

    Gather common project functionality

---
+++ gp-includes/project.php
@@ -0,0 +1,17 @@
+<?php
+class GP_Project {
+	
+	function get( &$project_or_id ) {
+		global $gpdb;
+		if ( is_object( $project_or_id ) )
+			return $project_or_id;
+		else
+			return $gpdb->get_row( $gpdb->prepare( "SELECT * FROM $gpdb->projects WHERE `id` = '%s'", $project_or_id ) );
+	}
+	
+	function get_by_path( $path ) {
+		global $gpdb;
+		$path = trim( $path, '/' );
+		return $gpdb->get_row( $gpdb->prepare( "SELECT * FROM $gpdb->projects WHERE path = '%s'", $path ) );
+	}
+}
