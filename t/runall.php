9c4eebd4512841203af6f1067e1aeb67253e6f90
Nikolay Bachiyski <nb@nikolay.bg>
2009-03-25 14:42:40 +0000

    Better caption for all test runner

--- t/runall.php
+++ t/runall.php
@@ -4,7 +4,7 @@ require_once('simpletest/unit_tester.php');
 require_once('simpletest/reporter.php');
 
 
-$test = &new TestSuite('All file tests');
+$test = &new TestSuite('All GloPress tests');
 
 foreach(glob('test_*.php') as $test_file)  {
     $test->addFile($test_file);
