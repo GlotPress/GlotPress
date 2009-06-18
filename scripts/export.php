ad046773c9c3278773b70c9c25ac873fe2f41835
Nikolay Bachiyski <nb@nikolay.bg>
2009-06-18 12:05:23 +0000

    Add encoding and version in exported file

--- scripts/export.php
+++ scripts/export.php
@@ -12,6 +12,10 @@ $translation_set = &GP_Translation_Set::by_project_id_slug_and_locale( $project-
 
 
 $po = new PO();
+
+$po->set_header('MIME-Version', '1.0');
+$po->set_header('Content-Transfer-Encoding', '8bit');
+$po->set_header('X-Generator', 'GlotPress/' . gp_get_option('version'));
 $po->set_header('Content-Type', 'text/plain; charset=UTF-8');
 $po->set_header('Plural-Forms', "nplurals=$locale->nplurals; plural=$locale->plural_expression;");
 $po->merge_with(GP_Translation::by_project_and_translation_set_and_status( $project, $translation_set, '+current' ));
