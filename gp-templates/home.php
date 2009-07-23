425f0104ae7e04f846ca448ad366f749017bc1f1
Nikolay Bachiyski <nb@nikolay.bg>
2009-07-23 17:14:06 +0000

    Proper permission check for create project link.

--- gp-templates/home.php
+++ gp-templates/home.php
@@ -7,5 +7,7 @@ gp_tmpl_header();
 <?php foreach($projects as $project): ?>
 	<li><?php gp_link_project( $project, gp_h( $project->name ) ); ?> <?php gp_link_project_edit( $project ); ?></li>
 <?php endforeach; ?>
-<p><?php gp_link( gp_url( '/project/_new' ), __('Create a New Project') ); ?></p>
+<?php if ( GP_User::current()->can( 'write', 'project' ) ): ?>
+	<p><?php gp_link( gp_url( '/project/_new' ), __('Create a New Project') ); ?></p>
+<?php endif; ?>
 <?php gp_tmpl_footer(); ?>
