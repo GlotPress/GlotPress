38cbb32cb25ff44c4ce72ebc30c58261a9c14cac
Nikolay Bachiyski <nb@nikolay.bg>
2009-07-23 21:54:15 +0000

    Add missing <ul>s.

--- gp-templates/home.php
+++ gp-templates/home.php
@@ -4,9 +4,11 @@ gp_breadcrumb(array( gp_link_home_get() ));
 gp_tmpl_header();
 ?>
 <h2>Projects</h2>
+<ul>
 <?php foreach($projects as $project): ?>
 	<li><?php gp_link_project( $project, gp_h( $project->name ) ); ?> <?php gp_link_project_edit( $project ); ?></li>
 <?php endforeach; ?>
+</ul>
 <?php if ( GP_User::current()->can( 'write', 'project' ) ): ?>
 	<p><?php gp_link( gp_url( '/project/_new' ), __('Create a New Project') ); ?></p>
 <?php endif; ?>
