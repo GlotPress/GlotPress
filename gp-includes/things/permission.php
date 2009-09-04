642efa2cb6c3574fa5df3b36fba92ba61ba3d61c
Nikolay Bachiyski <nb@nikolay.bg>
2009-09-04 18:14:11 +0000

    Remove commented code

--- gp-includes/things/permission.php
+++ gp-includes/things/permission.php
@@ -15,24 +15,5 @@ class GP_Permission extends GP_Thing {
 		}
 		return $args;
 	}
-
-	/*
-	function find( $args ) {
-		global $gpdb;
-		if ( !isset( $args['user_id'] ) ) {
-			$args['user_id'] = is_object( $args['user'] )? $args['user']->id : $args['user'];
-		}
-		
-		// TODO: load and cache all permissions of this user				
-		unset( $args['user'] );
-		$args_values_without_nulls = array_filter( array_values( $args ), create_function( '$v', 'return !is_null($v); ') );
-		$placeholder = create_function('$k', '$a = '.var_export($args, true).'; return is_null($a[$k])? "$k IS NULL" : "$k = %s";');
-		$where = implode(' AND ', array_map( $placeholder, array_keys( $args ) ) );
-		$query = $gpdb->prepare( "
-		    SELECT * FROM $gpdb->permissions
-		    WHERE $where", array_values( $args_values_without_nulls ) );
-		return $gpdb->get_row( $query );
-	}
-	*/
 }
 GP::$permission = new GP_Permission();
