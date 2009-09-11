c97cbf11950c3b0a3bc0de1816a2a785e4263fee
Nikolay Bachiyski <nb@nikolay.bg>
2009-09-11 19:32:08 +0000

    Forgot adding date_added to field list

--- gp-includes/things/translation.php
+++ gp-includes/things/translation.php
@@ -4,8 +4,7 @@ class GP_Translation extends GP_Thing {
 	var $per_page = 10;
 	
 	var $table_basename = 'translations';
-	var $field_names = array( 'id', 'original_id', 'translation_set_id', 'translation_0', 'translation_1', 'translation_2', 'translation_3',
-	 	'user_id', 'status' );
+	var $field_names = array( 'id', 'original_id', 'translation_set_id', 'translation_0', 'translation_1', 'translation_2', 'translation_3', 'user_id', 'status', 'date_added', );
 	var $non_updatable_attributes = array( 'id', );
 
 
