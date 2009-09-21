698350ce212fb283b8249ee89279d40a05f41c65
Nikolay Bachiyski <nb@nikolay.bg>
2009-09-21 15:58:42 +0000

    More usable translation set form

--- gp-templates/translation-set-form.php
+++ gp-templates/translation-set-form.php
@@ -4,13 +4,24 @@
 	
 	<!-- TODO: make slug edit WordPress style -->
 	<dt><label for="set[slug]"><?php _e('Slug');  ?></label></dt>
-	<dd><input type="text" name="set[slug]" value="<?php echo esc_html( $set->slug ); ?>" id="set[slug]"></dd>
+	<dd><input type="text" name="set[slug]" value="<?php echo esc_html( $set->slug? $set->slug : 'default' ); ?>" id="set[slug]"></dd>
 
 	<dt><label for="set[project_id]"><?php _e('Project');  ?></label></dt>
 	<dd><?php echo gp_select( 'set[project_id]', $all_project_options, $set->project_id); ?></dd>
 	
 	<dt><label for="set[locale]"><?php _e('Locale');  ?></label></dt>
-	<dd><?php echo gp_select( 'set[locale]', $all_locale_options, $set->locale); ?></dd>
+	<dd>
+		<?php echo gp_select( 'set[locale]', $all_locale_options, $set->locale); ?>
+		<a href="#" id="copy">Use as name</a>
+	</dd>
 	
 </dl>
 <?php echo gp_js_focus_on( 'set[name]' ); ?>
+<script type="text/javascript">
+	jQuery(function($){
+		$('#copy').click(function() {
+			$('#set\\[name\\]').val($('#set\\[locale\\] option:selected').html().replace(/^\S+\s+\S+\s+/, ''));
+			return false;
+		});
+	});
+</script>
