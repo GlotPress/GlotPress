e9c435de33d3ed854e61cce91c368957df7c20a2
Nikolay Bachiyski <nb@nikolay.bg>
2009-10-01 14:08:37 +0000

    Add script to import originald

---
+++ scripts/import-originals.php
@@ -0,0 +1,26 @@
+<?php
+require_once dirname( dirname( __FILE__ ) ) . '/gp-load.php';
+
+class GP_Script_Import_Originals extends GP_CLI {
+	
+	var $short_options = 'p:f:';
+	
+	var $usage = "-p <project-path> -f <pot-file>";
+	
+	function run() {
+		if ( !isset( $this->options['p'] ) ) {
+			$this->usage();
+		}
+		$project = GP::$project->by_path( $this->options['p'] );
+		if ( !$project ) $this->error( 'Project not found!' );
+		
+		$translations = new PO();
+		$translations->import_from_file( $this->options['f'] );
+		if ( !$translations ) $this->error( 'Error importing from POT file!' );
+		
+		GP::$original->import_for_project( $project, $translations );
+	}
+}
+
+$gp_script_import_originals = new GP_Script_Import_Originals;
+$gp_script_import_originals->run();
