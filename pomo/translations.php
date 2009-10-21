82d7bed0d0e9fdcad8d7cf162b5546f8ae6089aa
Nikolay Bachiyski <nb@nikolay.bg>
2009-10-21 05:46:08 +0000

    NOOP_Translations class to handle the cases we don't have a translation and be very fast

--- pomo/translations.php
+++ pomo/translations.php
@@ -202,3 +202,50 @@ class Gettext_Translations extends Translations {
 	}
 }
 endif;
+
+if ( !class_exists( 'NOOP_Translations' ) ):
+/**
+ * Provides the same interface as Translations, but doesn't do anything
+ */
+class NOOP_Translations {
+	var $entries = array();
+	var $headers = array();
+	
+	function add_entry($entry) {
+		return true;
+	}
+
+	function set_header($header, $value) {
+	}
+
+	function set_headers(&$headers) {
+	}
+
+	function get_header($header) {
+		return false;
+	}
+
+	function translate_entry(&$entry) {
+		return false;
+	}
+
+	function translate($singular, $context=null) {
+		return $singular;
+	}
+
+	function select_plural_form($count) {
+		return 1 == $count? 0 : 1;
+	}
+
+	function get_plural_forms_count() {
+		return 2;
+	}
+
+	function translate_plural($singular, $plural, $count, $context = null) {
+			return 1 == $count? $singular : $plural;
+	}
+
+	function merge_with(&$other) {
+	}
+}
+endif;
