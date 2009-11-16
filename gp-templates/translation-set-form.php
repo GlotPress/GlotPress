b437fdad1c152655c95c7644d32ef8718b927de6
Nikolay Bachiyski <nb@nikolay.bg>
2009-11-16 17:25:20 +0000

    Move locale field on top of the translation set form for better flow.

--- gp-templates/translation-set-form.php
+++ gp-templates/translation-set-form.php
@@ -1,4 +1,10 @@
 <dl>
+	<dt><label for="set[locale]"><?php _e('Locale');  ?></label></dt>
+	<dd>
+		<?php echo gp_select( 'set[locale]', $all_locale_options, $set->locale); ?>
+		<a href="#" id="copy">Use as name</a>
+	</dd>
+	
 	<dt><label for="set[name]"><?php _e('Name');  ?></label></dt>
 	<dd><input type="text" name="set[name]" value="<?php echo esc_html( $set->name ); ?>" id="set[name]"></dd>
 	
@@ -9,11 +15,6 @@
 	<dt><label for="set[project_id]"><?php _e('Project');  ?></label></dt>
 	<dd><?php echo gp_select( 'set[project_id]', $all_project_options, $set->project_id); ?></dd>
 	
-	<dt><label for="set[locale]"><?php _e('Locale');  ?></label></dt>
-	<dd>
-		<?php echo gp_select( 'set[locale]', $all_locale_options, $set->locale); ?>
-		<a href="#" id="copy">Use as name</a>
-	</dd>
 	
 </dl>
 <?php echo gp_js_focus_on( 'set[name]' ); ?>
