e6cf67bc96765a8182ee6c92aaf796b7249b8d29
Nikolay Bachiyski <nb@nikolay.bg>
2009-11-19 09:27:29 +0000

    Full-stop after the credits.

--- gp-templates/footer.php
+++ gp-templates/footer.php
@@ -1,5 +1,5 @@
 	<p id="footer"class="secondary">
-		Proudly powered by <a href="http://glotpress.org/" title="Found in translation">GlotPress</a>
+		Proudly powered by <a href="http://glotpress.org/" title="Found in translation">GlotPress</a>.
 		<?php gp_footer(); ?>		
 	</p>
 	</body>
