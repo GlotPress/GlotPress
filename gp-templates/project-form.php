fd23fcd5bc16c7b16c447bb0dd9581eda84ba60b
Nikolay Bachiyski <nb@nikolay.bg>
2010-02-18 09:05:57 +0000

    Show descriptive text for project description field in the project edit form

--- gp-templates/project-form.php
+++ gp-templates/project-form.php
@@ -9,13 +9,13 @@
 		<small><?php _e('If you leave the slug empty, it will be derived from the name.'); ?></small>
 	</dd>	
 
-	<dt><label for="project[description]"><?php _e('Description');  ?></label></dt>
+	<dt><label for="project[description]"><?php _e('Description');  ?></label> <span class="ternary">can include HTML</span></dt>
 	<dd><textarea name="project[description]" rows="4" cols="40" id="project[description]"><?php echo esc_html( $project->description ); ?></textarea></dd>
 
 	<dt><label for="project[source_url_template]"><?php _e('Source file URL');  ?></label></dt>
 	<dd>
 		<input type="text" value="<?php echo esc_html( $project->source_url_template ); ?>" name="project[source_url_template]" id="project[source_url_template]" style="width: 30em;" />
-		<small><?php _e('Public URL to a source file in the project. You can use <code>%file%</code> and <code>%line%</code>. Ex. <code>http://trac.example.org/browser/%file%#L%line%</code>'); ?></small>
+		<span class="ternary"><?php _e('Public URL to a source file in the project. You can use <code>%file%</code> and <code>%line%</code>. Ex. <code>http://trac.example.org/browser/%file%#L%line%</code>'); ?></span>
 	</dd>
 
 	<dt><label for="project[parent_project_id]"><?php _e('Parent Project');  ?></label></dt>
