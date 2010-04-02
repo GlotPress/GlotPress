e22493f34594896194bfdaf36db32475e045119f
Nikolay Bachiyski <nb@nikolay.bg>
2010-04-02 18:16:19 +0000

    Don't try to recurese permissions of there is no user

--- gp-includes/advanced-permissions.php
+++ gp-includes/advanced-permissions.php
@@ -5,7 +5,7 @@
  * has permissions on project parents
  */
 function gp_recurse_project_permissions( $verdict, $args ) {	
-	if ( $verdict || $args['object_type'] != 'project' || !$args['object_id'] ) return $verdict;
+	if ( $verdict || $args['object_type'] != 'project' || !$args['object_id'] || !$args['user'] ) return $verdict;
 	$project = GP::$project->get( $args['object_id'] );
 	if ( $project->parent_project_id ) {
 		return $args['user']->can( $args['action'], 'project', $project->parent_project_id );
