5ac2672c3c959c7ea697940652e257d9f0a175cd
Nikolay Bachiyski <nb@nikolay.bg>
2010-04-19 08:11:18 +0000

    Add actions before and after the login form

--- gp-templates/login.php
+++ gp-templates/login.php
@@ -5,6 +5,7 @@ gp_breadcrumb( array(
 ) );
 gp_tmpl_header();
 ?>
+	<?php do_action( 'before_login_form' ); ?>
 	<form action="<?php echo gp_url_ssl( gp_url_current() ); ?>" method="post">
 	<dl>
 		<dt><label for="user_login"><?php _e('Username'); ?></label></dt>
@@ -12,11 +13,11 @@ gp_tmpl_header();
 		
 		<dt><label for="user_pass"><?php _e('Password'); ?></label></dt>
 		<dd><input type="password" value="" id="user_pass" name="user_pass" /></dd>
-		
-		<dd><input type="submit" name="submit" value="<?php _e('Login'); ?>" id="submit"></dd>
 	</dl>
-	<input type="hidden" value="<?php echo esc_attr( gp_get( 'redirect_to' ) ); ?>" id="redirect_to" name="redirect_to" />	
+	<p><input type="submit" name="submit" value="<?php _e('Login'); ?>" id="submit"></p>
+	<input type="hidden" value="<?php echo esc_attr( gp_get( 'redirect_to' ) ); ?>" id="redirect_to" name="redirect_to" />
 </form>
+<?php do_action( 'after_login_form' ); ?>
 <script type="text/javascript" charset="utf-8">
 	document.getElementById('user_login').focus();
 </script>
