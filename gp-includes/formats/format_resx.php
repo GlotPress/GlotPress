8132e68de4556ea877ed3566272d951cc7551462
Nikolay Bachiyski <nb@nikolay.bg>
2010-11-22 23:31:28 +0000

    ResX format, first pass without comments

---
+++ gp-includes/formats/format_resx.php
@@ -0,0 +1,95 @@
+<?php
+
+class GP_Format_ResX {
+	
+	var $extension = 'resx.xml';
+	
+	var $exported = '';
+	
+	function line( $string, $prepend_tabs = 0 ) {
+		$this->exported .= str_repeat( "\t", $prepend_tabs ) . "$string\n";
+	}
+	
+	function res_header( $name, $value ) {
+		$this->line( '<resheader name="'.$name.'">', 1 );
+		$this->line( '<value>'.$value.'</value>', 2 );
+		$this->line( '</resheader>', 1 );		
+	}
+		
+	function print_exported_file( $project, $locale, $translation_set, $entries ) {
+		$this->exported = '';
+		$this->line( '<?xml version="1.0" encoding="utf-8"?>' );
+		$this->line( '<root>' );
+		$this->res_header( 'resmimetype', 'text/microsoft-resx' );
+		$this->res_header( 'version', '2.0' );
+		$this->res_header( 'reader', 'System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' );
+		$this->res_header( 'writer', 'System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' );
+		foreach( $entries as $entry ) {
+			if ( !preg_match( '/^[a-z0-9_]+$/', $entry->context ) ) {
+				error_log( 'ResX Export: Bad Entry: '. $entry->context );
+				continue;
+			}
+			$this->line( '<data name="' . $entry->context . '" xml:space="preserve">', 1 );
+			$this->line( '<value>' . $this->escape( $entry->translations[0] ) . '</value>', 2 );
+			$this->line( '</data>', 1 );
+		}
+		$this->line( '</root>' );
+		return $this->exported;
+	}
+	
+	function read_translations_from_file( $file_name, $project = null ) {
+		if ( is_null( $project ) ) return false;
+		$translations = $this->read_originals_from_file( $file_name );
+		if ( !$translations ) return false;
+		$originals = GP::$original->by_project_id( $project->id );
+		$new_translations = new Translations;
+		foreach( $translations->entries as $key => $entry ) {
+			// we have been using read_originals_from_file to parse the file
+			// so we need to swap singular and translation			
+			$entry->translations = array( $entry->singular );
+			$entry->singular = null;
+			foreach( $originals as $original ) {
+				if ( $original->context == $entry->context ) {
+					$entry->singular = $original->singular;
+					break;
+				}
+			}
+			if ( !$entry->singular ) {
+				error_log( sprintf( __("Missing context %s in project #%d"), $entry->context, $project->id ) );
+				continue;
+			}
+			
+			$new_translations->add_entry( $entry );
+		}
+		return $new_translations;
+		
+	}
+
+	function read_originals_from_file( $file_name ) {
+		$errors = libxml_use_internal_errors( 'true' );
+		$data = simplexml_load_string( file_get_contents( $file_name ) );
+		libxml_use_internal_errors( $errors );
+		if ( !is_object( $data ) ) return false;
+		$entries = new Translations;
+		foreach( $data->data as $string ) {
+			$entry = new Translation_Entry();
+			$entry->context = (string)$string['name'];
+			$entry->singular = $this->unescape( (string)$string->value );
+			$entry->translations = array();
+			$entries->add_entry( $entry );
+		}
+		return $entries;
+	}
+
+	
+	function unescape( $string ) {
+		return $string;
+	}
+	
+	function escape( $string ) {
+		$string = str_replace( array( '&', '<' ), array( '&amp;', '&lt;' ), $string );
+		return $string;
+	}
+}
+
+GP::$formats['android'] = new GP_Format_Android;
--- t/unittests-config.php
+++ t/unittests-config.php
@@ -2,7 +2,7 @@
 define('GPDB_NAME', 'glotpress_test');
 define('GPDB_USER', 'root');
 define('GPDB_PASSWORD', 'root');
-define('GPDB_HOST', 'localhost');
+define('GPDB_HOST', '127.0.0.1');
 define('GPDB_CHARSET', 'utf8');
 define('GPDB_COLLATE', '');
 $gp_table_prefix = 'gp_';
