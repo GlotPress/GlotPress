98ed1f9a27b641f02e8418e5d8f5c5ae816b7dcd
Nikolay Bachiyski <nb@nikolay.bg>
2011-05-13 07:29:06 +0000

    Remove my specific unit tests config

--- t/unittests-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('GPDB_NAME', 'glotpress_test');
-define('GPDB_USER', 'root');
-define('GPDB_PASSWORD', 'root');
-define('GPDB_HOST', '127.0.0.1');
-define('GPDB_CHARSET', 'utf8');
-define('GPDB_COLLATE', '');
-$gp_table_prefix = 'gp_';
-?>
