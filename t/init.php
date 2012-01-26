9ccc2cef6bc6ee1ff6dac1b4b8cfb0731a2dc239
Nikolay Bachiyski <nb@nikolay.bg>
2012-01-26 12:47:01 +0000

    Set the protocol to HTTP/1.1 for CLI tests

--- t/init.php
+++ t/init.php
@@ -3,6 +3,10 @@ require_once 'PHPUnit/Autoload.php';
 
 define( 'GP_CONFIG_FILE', dirname( __FILE__ ) . '/unittests-config.php' );
 
+if ( !isset( $_SERVER['SERVER_PROTOCOL'] ) ) {
+	$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+}
+
 require_once dirname( __FILE__ ) . '/../gp-load.php';
 
 require dirname( __FILE__ ) . '/lib/testcase.php';
