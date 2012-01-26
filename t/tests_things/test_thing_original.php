813f491f9ebdf39224fb63011b0cfa817058b3d8
Nikolay Bachiyski <nb@nikolay.bg>
2012-01-26 13:46:22 +0000

    Add and update some tests
     * Because of the different update implementation (see [657]), the number of update() calls changed
     * Add tests for the problem described in #173
     * Pass proper {{{Translations}}} object to {{{import_for_project()}}}, instead of hacked one. This way
    the {{{entries}}} array will have keys, based on the content and not just numeric ones.

--- t/tests_things/test_thing_original.php
+++ t/tests_things/test_thing_original.php
@@ -24,28 +24,54 @@ class GP_Test_Thing_Original extends GP_UnitTestCase {
 		$original->reload();
 		return array( $project, $original );
 	}
+	
+	function create_translations_with( $entries ) {
+		$translations = new Translations;
+		foreach( $entries as $entry ) {
+			$translations->add_entry( $entry );
+		}
+		return $translations;
+	}
 
 	function test_import_for_project_should_not_update_unchanged_originals() {
 		list( $project, $original ) = $this->create_original_with_update_counter();
-		$original->import_for_project( $project, (object)array('entries' => array(new Translation_Entry( array('singular' => $original->singular) ) ) ) );
-		$this->assertEquals( 2, $GLOBALS['update_invocation_count'], 'update should be invoked only 2 times' );
+		$translations = $this->create_translations_with( array( array( 'singular' => $original->singular ) ) );
+		$original->import_for_project( $project, $translations );
+		$this->assertEquals( 0, $GLOBALS['update_invocation_count'], 'update should be invoked only 2 times' );
 	}
 	
 	function test_import_for_project_should_update_changed_originals() {
 		list( $project, $original ) = $this->create_original_with_update_counter( array(
 			'comment' => 'Some comment'
 		) );
-		$original->import_for_project( $project, (object)array('entries' => array(new Translation_Entry( array('singular' => $original->singular ) ) ) ) );
-		$this->assertEquals( 3, $GLOBALS['update_invocation_count'], 'update should be invoked 3 times' );
+		$translations = $this->create_translations_with( array( array( 'singular' => $original->singular ) ) );
+		$original->import_for_project( $project, $translations );
+		$this->assertEquals( 1, $GLOBALS['update_invocation_count'], 'update should be invoked 3 times' );
 	}	
 	
 	function test_should_be_updated_with_should_return_true_if_only_singular_is_for_update_and_it_is_the_same() {
 		$original = $this->factory->original->create();
-		$this->assertFalse( GP::$original->should_be_updated_with( $original, array( 'singular' => $original->singular ) ) );
+		$this->assertFalse( GP::$original->should_be_updated_with( array( 'singular' => $original->singular ), $original ) );
 	}
 	
 	function test_should_be_updated_with_should_return_true_if_one_value_is_empty_string_and_the_other_is_null() {
 		$original = $this->factory->original->create( array( 'comment' => NULL ) );
-		$this->assertFalse( GP::$original->should_be_updated_with( $original, array( 'singular' => $original->singular, 'comment' => '' ) ) );
+		$this->assertFalse( GP::$original->should_be_updated_with( array( 'singular' => $original->singular, 'comment' => '' ), $original ) );
+	}
+	
+	function test_should_be_updated_with_should_use_this_if_second_argument_is_not_supplied() {
+		$original = $this->factory->original->create();
+		$data = array( 'singular' => 'baba' );
+		$this->assertEquals( GP::$original->should_be_updated_with( $data, $original ), $original->should_be_updated_with( $data )  );
+	}
+	
+	function test_import_should_leave_unchanged_strings_as_active() {
+		$project = $this->factory->project->create();
+		$original = $this->factory->original->create( array( 'project_id' => $project->id, 'status' => '+active', 'singular' => 'baba' ) );
+		$translations = $this->create_translations_with( array( array( 'singular' => 'baba' ) ) );
+		$original->import_for_project( $project, $translations );
+		$originals_for_project = $original->by_project_id( $project->id );
+		$this->assertEquals( 1, count( $originals_for_project ) );
+		$this->assertEquals( 'baba', $originals_for_project[0]->singular );
 	}
 }
