e4ff2a1bbf1ab4f2e4ce222b616fafcd0f37bdb5
Andrew Nacin <andrewnacin@gmail.com>
2012-04-06 15:13:36 +0000

    Update assets-loader for jQuery and jQuery UI. props petervanderdoes. see [673] for jQuery and jQuery UI. fixes #179, fixes #180.

--- gp-includes/assets-loader.php
+++ gp-includes/assets-loader.php
@@ -5,9 +5,9 @@
 
 function gp_styles_default( &$styles ) {
 	$styles->base_url = gp_url_base( 'css' );
-    $styles->default_version = gp_get_option( 'version' );
+	$styles->default_version = gp_get_option( 'version' );
 	// TODO: get text direction for current locale
-    //$styles->text_direction = 'rtl' == get_bloginfo( 'text_direction' ) ? 'rtl' : 'ltr';
+	//$styles->text_direction = 'rtl' == get_bloginfo( 'text_direction' ) ? 'rtl' : 'ltr';
 	$styles->text_direction = 'ltr';
 	
 	$styles->add( 'base', '/style.css', array(), '20100812' );
@@ -17,17 +17,19 @@ add_action( 'wp_default_styles', 'gp_styles_default' );
 
 function gp_scripts_default( &$scripts ) {
 	$scripts->base_url = gp_url_base( 'js' );
-    $scripts->default_version = gp_get_option( 'version' );
-	
-	$scripts->add( 'jquery', '/jquery/jquery.js', array(), '1.4.2-min' );
-	$scripts->add( 'jquery-ui', '/jquery/jquery-ui.js', array('jquery'), '1.8-0' );
-	$scripts->add( 'jquery-ui-autocomplete', null, array('jquery-ui'), '1.8' );
-	$scripts->add( 'jquery-ui-selectable', null, array('jquery-ui'), '1.8' );
-	$scripts->add( 'jquery-ui-tabs', null, array('jquery-ui'), '1.8' );
-	$scripts->add( 'common', '/common.js', array( 'jquery' ), '20100812' );
-	$scripts->add( 'editor', '/editor.js', array( 'common', 'google-js-api' ), '20100715' );
-	$scripts->add( 'translations-page', '/translations-page.js', array( 'common' ), '20100423' );
-	$scripts->add( 'mass-create-sets-page', '/mass-create-sets-page.js', array( 'common' ), '20100423' );
+	$scripts->default_version = gp_get_option( 'version' );
+
+	$bump = '20120406';
+
+	$scripts->add( 'jquery', '/jquery/jquery.js', array(), '1.7.2' );
+	$scripts->add( 'jquery-ui', '/jquery/jquery-ui.js', array('jquery'), '1.8.18' );
+	$scripts->add( 'jquery-ui-autocomplete', null, array('jquery-ui'), '1.8.18' );
+	$scripts->add( 'jquery-ui-selectable', null, array('jquery-ui'), '1.8.18' );
+	$scripts->add( 'jquery-ui-tabs', null, array('jquery-ui'), '1.8.18' );
+	$scripts->add( 'common', '/common.js', array( 'jquery' ), $bump );
+	$scripts->add( 'editor', '/editor.js', array( 'common', 'google-js-api' ), $bump );
+	$scripts->add( 'translations-page', '/translations-page.js', array( 'common' ), $bump );
+	$scripts->add( 'mass-create-sets-page', '/mass-create-sets-page.js', array( 'common' ), $bump );
 	$scripts->add( 'google-js-api', 'http://www.google.com/jsapi', array(), '' );
 }
 
