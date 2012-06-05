a95107da38d9c7e3947361fcdf6a3e0dc7140f42
Nikolay Bachiyski <nb@nikolay.bg>
2012-06-05 11:25:42 +0000

    Remove trailing whitespace

--- t/test_warnings.php
+++ t/test_warnings.php
@@ -11,20 +11,20 @@ class GP_Test_Translation_Warnings extends GP_UnitTestCase {
 		$this->with_equal->add( 'is_equal', $this->is_equal );
 		$this->standard_plural_locale = $this->factory->locale->create();
 	}
-	
+
 	function test_add() {
 		$this->w->add( 'is_baba', $this->is_baba );
 		$this->assertEquals( true, $this->w->has( 'is_baba' ) );
 		$this->assertEquals( false, $this->w->has( 'is_dyado' ) );
 	}
-	
+
 	function test_remove() {
 		$this->w->add( 'is_baba', $this->is_baba );
 		$this->assertEquals( true, $this->w->has( 'is_baba' ) );
 		$this->w->remove( 'is_baba' );
 		$this->assertEquals( false, $this->w->has( 'is_baba' ) );
 	}
-	
+
 	function test_check() {
 		$this->w->add( 'is_baba', $this->is_baba );
 		$this->assertEquals( array(
@@ -33,7 +33,7 @@ class GP_Test_Translation_Warnings extends GP_UnitTestCase {
 		$this->assertEquals( null,
 			$this->w->check( 'baba', null, array('баба', 'баба', 'баба'), $this->standard_plural_locale ) );
 	}
-	
+
 	/**
 	 * For the plural form, corresponding to the number 1 check only against the singular, not also against the plural
 	 */
@@ -45,7 +45,7 @@ class GP_Test_Translation_Warnings extends GP_UnitTestCase {
 		$this->assertEquals( array(0 => array('is_equal' => 'error')),
 			$this->with_equal->check( 'baba', 'babas', array('babas', 'babas'), $this->standard_plural_locale ) );
 	}
-	
+
 	/**
 	 * If a locale has no plural forms, we should check only against the plural original, since
 	 * it probably contains the placeholders
@@ -60,6 +60,6 @@ class GP_Test_Translation_Warnings extends GP_UnitTestCase {
 			$this->with_equal->check( 'baba', 'babas', array('baba'), $no_plural_locale ) );
 		$this->assertEquals( array(0 => array('is_equal' => 'error')),
 			$this->with_equal->check( 'baba', 'babas', array('xxx'), $no_plural_locale ) );
-			
+
 	}
 }
