d232339649a1d91727fa1254ba9c1cb8a78b137d
Marko Heijnen <info@markoheijnen.com>
2013-06-17 11:57:53 +0000

    Update footer id to "gp-footer". See #244

--- gp-templates/footer.php
+++ gp-templates/footer.php
@@ -1,5 +1,5 @@
 	</div><!-- .gp-content -->
-	<p id="footer" class="secondary">
+	<p id="gp-footer" class="secondary">
 		<?php printf( __('Proudly powered by <a href="%s" title="Found in translation">GlotPress</a>.'), 'http://glotpress.org/' ); ?>
 		<?php gp_footer(); ?>
 	</p>
