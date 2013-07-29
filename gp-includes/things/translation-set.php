8f172a843583c48df94d5ecf3501eeedf095e37f
Marko Heijnen <info@markoheijnen.com>
2013-07-29 23:02:51 +0000

    Reverted most of the code of #233. wp_locale isn't something GlotPress in general should be dealing with

--- gp-includes/things/translation-set.php
+++ gp-includes/things/translation-set.php
@@ -3,17 +3,8 @@ class GP_Translation_Set extends GP_Thing {
 
 	var $table_basename = 'translation_sets';
 	var $field_names = array( 'id', 'name', 'slug', 'project_id', 'locale' );
-	var $non_db_field_names = array( 'wp_locale', 'current_count', 'untranslated_count', 'waiting_count' ); 
+	var $non_db_field_names = array( 'current_count', 'untranslated_count', 'waiting_count' ); 
 	var $non_updatable_attributes = array( 'id' );
-
-	function __construct( $fields = array() ) {
-		parent::__construct( $fields );
-
-		$locale = GP_Locales::by_slug( $this->locale );
-
-		if( $locale )
-			$this->wp_locale = $locale->wp_locale;
-	}
 
 	function restrict_fields( $set ) {
 		$set->name_should_not_be('empty');
