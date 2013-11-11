010ddf47e90d3144e1fd08583e24ddec5e5b2379
Marko Heijnen <info@markoheijnen.com>
2013-11-11 16:26:17 +0000

    API calls now always return JSON data. Even after a 404. Fixes #272

--- gp-includes/template.php
+++ gp-includes/template.php
@@ -59,7 +59,14 @@ function gp_tmpl_filter_args( $args ) {
 }
 
 function gp_tmpl_404( $args = array()) {
-	gp_tmpl_load( '404', $args + array('title' => __('Not Found'), 'http_status' => 404 ) );
+	if( GP::$current_route->api  ) {
+		status_header(404);
+		echo json_encode( array( 'success' => false, 'error' => __('Not Found') ) );
+	}
+	else {
+		gp_tmpl_load( '404', $args + array('title' => __('Not Found'), 'http_status' => 404 ) );
+	}
+
 	exit();
 }
 
