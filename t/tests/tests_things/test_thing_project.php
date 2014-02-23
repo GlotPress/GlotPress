2bf6ab2b2c22bc0139e8a86d7d3fdc0bff646a5b
Marko Heijnen <info@markoheijnen.com>
2014-02-23 21:58:18 +0000

    Add more unit tests for GP_Project. Checking required fields, regenerate_paths() and set_difference_sets(). See #286

--- t/tests/tests_things/test_thing_project.php
+++ t/tests/tests_things/test_thing_project.php
@@ -1,6 +1,22 @@
 <?php
 
 class GP_Test_Project extends GP_UnitTestCase {
+
+	function test_empty_name() {
+		$project = GP::$project->create( array( 'name' => '' ) );
+		$verdict = $project->validate();
+
+		$this->assertFalse( $verdict );
+	}
+
+	function test_empty_slug() {
+		$project = GP::$project->create( array( 'name' => 'Name', 'slug' => false ) );
+		$verdict = $project->validate();
+
+		$this->assertTrue( $verdict );
+		$this->assertEquals( 'name', $project->path );
+	}
+
 	function test_update_path() {
 		$root = GP::$project->create( array( 'name' => 'Root', 'slug' => 'root', 'path' => 'root' ) );
 		// the slug is changed
@@ -28,15 +44,15 @@ class GP_Test_Project extends GP_UnitTestCase {
 		$this->assertEquals( 'invader', $q->path );
 	}
 
-	function test_create_and_select() { 
-		$project = new GP_Project( array( 'name' => '@@@@', 'slug' => '' ) ); 
-		$verdict = $project->validate(); 
+	function test_create_and_select() {
+		$project = new GP_Project( array( 'name' => '@@@@', 'slug' => '' ) );
+		$verdict = $project->validate();
 
-		$this->assertFalse( $verdict ); 
-	} 
+		$this->assertFalse( $verdict );
+	}
 
 	function test_save_no_args() {
-		$p1 = GP::$project->create( array( 'name' => 'P1', 'slug' => 'p1', 'path' => 'p1', ) );
+		$p1 = GP::$project->create( array( 'name' => 'P1', 'slug' => 'p1', 'path' => 'p1' ) );
 		$id = $p1->id;
 		$p1->name = 'P2';
 		$p1->save();
@@ -48,7 +64,7 @@ class GP_Test_Project extends GP_UnitTestCase {
 
 	function test_reload() {
 		global $gpdb;
-		$root = GP::$project->create( array( 'name' => 'Root', 'slug' => 'root'  ) );
+		$root = GP::$project->create( array( 'name' => 'Root', 'slug' => 'root' ) );
 		$gpdb->update( $gpdb->projects, array( 'name' => 'Buuu' ), array( 'id' => $root->id ) );
 		$root->reload();
 		$this->assertEquals( 'Buuu', $root->name );
@@ -62,4 +78,36 @@ class GP_Test_Project extends GP_UnitTestCase {
 		$this->assertEquals( array( $sub, $root ), $sub->path_to_root() );
 		$this->assertEquals( array( $root ), $root->path_to_root() );
 	}
+
+	function test_regenerate_paths() {
+		global $gpdb;
+		$root = GP::$project->create( array( 'name' => 'Root', 'slug' => 'root' ) );
+		$sub  = $this->factory->project->create( array( 'name' => 'Sub', 'parent_project_id' => $root->id ) );
+		$gpdb->update( $gpdb->projects, array( 'path' => 'wrong-path' ), array( 'id' => $sub->id ) );
+		$sub->reload();
+		$sub->regenerate_paths();
+		$sub->reload();
+		$this->assertEquals( 'root/sub', $sub->path );
+	}
+
+	function test_set_difference_from_same() {
+		$p1 = $this->factory->project->create( array( 'name' => 'P1' ) );
+		$p2 = $this->factory->project->create( array( 'name' => 'P2' ) );
+
+		$difference = $p1->set_difference_from( $p2 );
+
+		$this->assertEmpty( $difference['added'] );
+		$this->assertEmpty( $difference['removed'] );
+	}
+
+	function test_set_difference_from_difference() {
+		$s1 = $this->factory->translation_set->create_with_project_and_locale( array( 'locale' => 'bg' ), array( 'name' => 'P1' ) );
+		$s2 = $this->factory->translation_set->create_with_project_and_locale( array( 'locale' => 'nl' ), array( 'name' => 'P2' ) );
+
+		$difference = $s1->project->set_difference_from( $s2->project );
+
+		$this->assertEquals( $s2->id, $difference['added'][0]->id );
+		$this->assertEquals( $s1->id, $difference['removed'][0]->id );
+	}
+
 }
