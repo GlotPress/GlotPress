09de30ee294053b8f05ec355445ca1e8e74324df
Yoav Farhi <yoav@automattic.com>
2014-03-25 08:53:43 +0000

    glossary: no need to test reload, since we have no methods that update the glossary directly in our class. props nbachiyski
    See #97

--- t/tests/tests_things/test_thing_glossary.php
+++ t/tests/tests_things/test_thing_glossary.php
@@ -9,14 +9,6 @@ class GP_Test_Glossary extends GP_UnitTestCase {
 		$this->assertFalse( $verdict );
 	}
 
-	function test_reload() {
-		global $gpdb;
-		$root = GP::$glossary->create( array( 'translation_set_id' => '1', 'description' => 'original description' ) );
-		$gpdb->update( $gpdb->glossaries, array( 'description' => 'New Description' ), array( 'id' => $root->id ) );
-		$root->reload();
-		$this->assertEquals( 'New Description', $root->description );
-	}
-
 	function test_by_set_id() {
 		$glossary_1 = GP::$glossary->create_and_select( array( 'translation_set_id' => '1' ) );
 		$glossary_2 = GP::$glossary->create_and_select( array( 'translation_set_id' => '2' ) );
