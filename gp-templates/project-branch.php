ae4f294f30ca3de9167c44736131c04fa76f447c
Yoav Farhi <yoav@automattic.com>
2014-04-29 06:46:07 +0000

    Branching: allow editing of the project description. See #65

--- gp-templates/project-branch.php
+++ gp-templates/project-branch.php
@@ -15,13 +15,14 @@ gp_tmpl_header();
 		<input type="text" name="project[slug]" value="" id="project[slug]">
 		<small><?php _e('If you leave the slug empty, it will be derived from the name.'); ?></small>
 	</dd>
+	<dt><label for="project[description]"><?php _e('Description'); ?></label> <span class="ternary"><?php _e('can include HTML'); ?></span></dt>
+	<dd><textarea name="project[description]" rows="4" cols="40" id="project[description]"><?php echo esc_html( $project->description ); ?></textarea></dd>
 	<dt><label for="project[source_url_template]"><?php _e('Source file URL'); ?></label></dt>
 	<dd>
 		<input type="text" value="<?php echo esc_html( $project->source_url_template ); ?>" name="project[source_url_template]" id="project[source_url_template]" style="width: 30em;" />
 		<span class="ternary"><?php _e('Public URL to a source file in the project. You can use <code>%file%</code> and <code>%line%</code>. Ex. <code>http://trac.example.org/browser/%file%#L%line%</code>'); ?></span>
 	</dd>
 	<div id="preview"></div>
-	<input type="hidden" value="<?php echo esc_html( $project->description ); ?>" name="project[description]" id="project[description]" />
 	<input type="hidden" value="<?php echo esc_html( $project->parent_project_id ); ?>" name="project[parent_project_id]" id="project[parent_project_id]" />
 	<p><input type="submit" name="submit" value="<?php echo esc_attr( __('Branch project') ); ?>" id="submit" /></p>
 </form>
