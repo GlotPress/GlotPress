671b9ad8d3f62355000fda0710e775bd03833b5f
Marko Heijnen <info@markoheijnen.com>
2014-05-09 18:43:54 +0000

    Return all die_with_404() and die_with_error() calls in GP_Route_Original. See #321

--- gp-includes/routes/original.php
+++ gp-includes/routes/original.php
@@ -2,18 +2,29 @@
 
 class GP_Route_Original extends GP_Route_Main {
 
-    function set_priority( $original_id ) {
-        $original = GP::$original->get( $original_id );
-        if ( !$original ) $this->die_with_404();
-        $project = GP::$project->get( $original->project_id );
-        if ( !$project ) $this->die_with_404();
-        $this->can_or_forbidden( 'write', 'project', $project->id );
-        $original->priority = gp_post( 'priority' );
-        if ( !$original->validate() ) {
-            $this->die_with_error( 'Invalid priority value!' );
-        }
-        if ( !$original->save() ) {
-            $this->die_with_error( 'Error in saving original!' );
-        }
-    }
+	function set_priority( $original_id ) {
+		$original = GP::$original->get( $original_id );
+
+		if ( ! $original ) {
+			return $this->die_with_404();
+		}
+
+		$project = GP::$project->get( $original->project_id );
+
+		if ( ! $project ) {
+			return $this->die_with_404();
+		}
+
+		$this->can_or_forbidden( 'write', 'project', $project->id );
+		$original->priority = gp_post( 'priority' );
+
+		if ( ! $original->validate() ) {
+			return $this->die_with_error( 'Invalid priority value!' );
+		}
+
+		if ( ! $original->save() ) {
+			return $this->die_with_error( 'Error in saving original!' );
+		}
+	}
+
 }
