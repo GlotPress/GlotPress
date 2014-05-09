56909bf7283488a062f51f929d149fdf5f274c53
Marko Heijnen <info@markoheijnen.com>
2014-05-09 19:37:01 +0000

    Removed unused $errors variable in GP_Project. See #323.

--- gp-includes/routes/project.php
+++ gp-includes/routes/project.php
@@ -337,7 +337,7 @@ class GP_Route_Project extends GP_Route_Main {
 		}
 
 		$changes = $project->set_difference_from( $other_project );
-		$errors = 0;
+
 		foreach( $changes['added'] as $to_add ) {
 			if ( !GP::$translation_set->create( array('project_id' => $project->id, 'name' => $to_add->name, 'locale' => $to_add->locale, 'slug' => $to_add->slug) ) ) {
 				$this->errors[] = __('Couldn&#8217;t add translation set named %s', esc_html( $to_add->name ) );
