b9a14c56d46e3d70a5756c9e4af53179011872f6
Marko Heijnen <info@markoheijnen.com>
2014-05-09 19:38:51 +0000

    Remove break; after a return in BP_Options. See #323.

--- gp-includes/class.bp-options.php
+++ gp-includes/class.bp-options.php
@@ -14,28 +14,20 @@ class BP_Options
 		switch ( $option ) {
 			case 'application_id':
 				return 'glotpress';
-				break;
 			case 'application_uri':
 				return gp_url();
-				break;
 			case 'cron_uri':
 				return '';
-				break;
 			case 'cron_check':
 				return '';
-				break;
 			case 'charset':
 				return 'UTF-8';
-				break;
 			case 'wp_http_version':
 				return 'GlotPress/' . gp_get_option('version');
-				break;
 			case 'hash_function_name':
 				return 'gp_hash';
-				break;
 			default:
 				return gp_get_option( BP_Options::prefix() . $option );
-				break;
 		}
 	}
 
@@ -50,4 +42,5 @@ class BP_Options
 	public static function delete($option) {
 		return bb_delete_option( BP_Options::prefix() . $option );
 	}
+
 } // END class BP_Options
