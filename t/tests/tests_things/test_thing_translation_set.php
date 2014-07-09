8ab6c185b803f367895fb57ac8e60071b2f656d8
Yoav Farhi <yoav@automattic.com>
2014-07-09 07:04:05 +0000

    Unit tests for GP_Translation_Set::import()
    Fixes #341

--- t/tests/tests_things/test_thing_translation_set.php
+++ t/tests/tests_things/test_thing_translation_set.php
@@ -48,5 +48,45 @@ class GP_Test_Thing_Translation_set extends GP_UnitTestCase {
 		$this->assertEquals( $translations[1]->status, 'current' );
 	}
 
-}
+	function test_import_should_not_import_existing_same_translation() {
+		$set = $this->factory->translation_set->create_with_project_and_locale();
+		$original = $this->factory->original->create( array( 'project_id' => $set->project->id, 'status' => '+active', 'singular' => 'A string' ) );
+		$translation = $this->factory->translation->create( array( 'translation_set_id' => $set->id, 'original_id' => $original->id, 'translations' => array( 'baba' ), 'status' => 'current' ) );
 
+		$translations_for_import = new Translations;
+		$translations_for_import->add_entry( array( 'singular' => 'A string', 'translations' => array( 'baba' ) ) );
+		$translations_added = $set->import( $translations_for_import );
+
+		$this->assertEquals( $translations_added, 0 );
+
+	}
+
+	function test_import_should_import_over_existing_different_translation_by_default() {
+		$set = $this->factory->translation_set->create_with_project_and_locale();
+		$original = $this->factory->original->create( array( 'project_id' => $set->project->id, 'status' => '+active', 'singular' => 'A string' ) );
+		$translation = $this->factory->translation->create( array( 'translation_set_id' => $set->id, 'original_id' => $original->id, 'translations' => array( 'baba' ), 'status' => 'current' ) );
+
+		$translations_for_import = new Translations;
+		$translations_for_import->add_entry( array( 'singular' => 'A string', 'translations' => array( 'abab' ) ) );
+		$translations_added = $set->import( $translations_for_import );
+
+		$this->assertEquals( $translations_added, 1 );
+
+	}
+
+	function test_filter_translation_set_import_over_existing() {
+		$set = $this->factory->translation_set->create_with_project_and_locale();
+		$original = $this->factory->original->create( array( 'project_id' => $set->project->id, 'status' => '+active', 'singular' => 'A string' ) );
+		$translation = $this->factory->translation->create( array( 'translation_set_id' => $set->id, 'original_id' => $original->id, 'translations' => array( 'baba' ), 'status' => 'current' ) );
+
+		$translations_for_import = new Translations;
+		$translations_for_import->add_entry( array( 'singular' => 'A string', 'translations' => array( 'abab' ) ) );
+
+		add_filter( 'translation_set_import_over_existing', '__return_false' );
+		$translations_added = $set->import( $translations_for_import );
+		remove_filter( 'translation_set_import_over_existing', '__return_false' );
+
+		$this->assertEquals( $translations_added, 0 );
+	}
+
+}
