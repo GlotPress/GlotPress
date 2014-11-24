f5862e35edb85c5c4a92aa32ba8316dba4d25fae
Marko Heijnen <info@markoheijnen.com>
2014-11-24 20:49:35 +0000

    Don't flush the translation cache when copying a translation set. See #208

--- gp-includes/things/translation.php
+++ gp-includes/things/translation.php
@@ -12,7 +12,7 @@ class GP_Translation extends GP_Thing {
 	function create( $args ) {
 		$inserted = parent::create( $args );
 
-		if ( $inserted ) {
+		if ( $inserted && is_array( $args ) ) {
 			gp_clean_translation_set_cache( $args['translation_set_id'] );
 		}
 
