e3f9a07cd79bb2410170354e6c092df2f2ab693c
Marko Heijnen <info@markoheijnen.com>
2015-07-17 13:52:15 +0000

    Use wp_list_pluck to get all the project_id's in GP_Route_Locale.
    See #468

--- gp-includes/routes/locale.php
+++ gp-includes/routes/locale.php
@@ -26,11 +26,7 @@ class GP_Route_Locale extends GP_Route_Main {
 		usort( $sets, array( $this, 'sort_sets_by_project_id' ) );
 
 		$locale_projects = $projects_data = $projects = $parents = $set_slugs = $set_list = array();
-
-		//TODO: switch to wp_list_pluck
-		foreach ( $sets as $key => $value ) {
-			$locale_projects[ $key ] = $value->project_id;
-		}
+		$locale_projects = wp_list_pluck( $sets, 'project_id' );
 
 		foreach ( $sets as $set ) {
 			$set_slugs[ $set->slug ] = $set;
