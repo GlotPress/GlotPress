c04c9c49f92009af4e991088b1886f35309f774c
Ashley Rich <hello@ashleyrich.com>
2015-09-25 12:56:33 +0100

    Remove user logged_in method

--- gp-includes/things/user.php
+++ gp-includes/things/user.php
@@ -40,13 +40,8 @@ class GP_User extends GP_Thing {
 		return $args;
 	}
 
-	function logged_in() {
-		$coerced = $this->coerce( wp_get_current_user() );
-		return ( $coerced && $coerced->id );
-	}
-
 	function current() {
-		if ( $this->logged_in() )
+		if ( is_user_logged_in() )
 			return $this->coerce( wp_get_current_user() );
 		else
 			return new GP_User( array( 'id' => 0, ) );
@@ -94,7 +89,7 @@ class GP_User extends GP_Thing {
 		$user = null;
 		if ( isset( $this ) && $this->id )
 			$user = $this;
-		elseif ( GP::$user->logged_in() )
+		elseif ( is_user_logged_in() )
 			$user = GP::$user->current();
 		$user_id = $user? $user->id : null;
 		$args = $filter_args = compact( 'user_id', 'action', 'object_type', 'object_id' );
