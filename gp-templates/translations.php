3c1fad06162b127d190c82adf2db8b8fef6ae47f
Greg Ross <greg@toolstack.com>
2015-10-16 08:21:57 -0400

    Fix wp_localize_script handle names that were missed during the initial update.

--- gp-templates/translations.php
+++ gp-templates/translations.php
@@ -12,7 +12,7 @@ wp_localize_script( 'gp-translations-page', '$gp_translations_options', array( '
 // localizer adds var in front of the variable name, so we can't use $gp.editor.options
 $editor_options = compact('can_approve', 'can_write', 'url', 'discard_warning_url', 'set_priority_url', 'set_status_url');
 
-wp_localize_script( 'editor', '$gp_editor_options', $editor_options );
+wp_localize_script( 'gp-editor', '$gp_editor_options', $editor_options );
 
 gp_tmpl_header();
 $i = 0;
