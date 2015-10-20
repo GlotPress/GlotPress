9fc67a290118484892453635cf0af1f5cf53e954
Greg Ross <greg@toolstack.com>
2015-10-20 10:32:16 -0400

    Add delete project link.

--- gp-templates/project-edit.php
+++ gp-templates/project-edit.php
@@ -8,6 +8,7 @@ gp_tmpl_header();
 <?php gp_tmpl_load( 'project-form', get_defined_vars()); ?>
 	<p>
 		<input type="submit" name="submit" value="<?php echo esc_attr( __('Save') ); ?>" id="submit" />
+		<span class="or-delete"><?php _e('or'); ?> <a href="<?php echo gp_url_project( $project ) . '/-delete'; ?>" onclick="return confirm('<?php _e('Are you sure you want to delete this project?');?>');"><?php _e('Delete'); ?></a></span>
 		<span class="or-cancel"><?php _e('or'); ?> <a href="javascript:history.back();"><?php _e('Cancel'); ?></a></span>
 	</p>
 </form>
