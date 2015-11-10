a786ad557b5c81285118c0db022262b7ac4bd32c
Dominik Schilling <dominikschilling+git@gmail.com>
2015-11-10 22:15:03 +0100

    Remove references to `clean_url()`.
    `clean_url()` is now `esc_url()` (or `esc_url_raw()`) which can handle more characters, like square brackets.
    
    Fixes #147.

--- gp-includes/routes/translation.php
+++ gp-includes/routes/translation.php
@@ -263,8 +263,7 @@ class GP_Route_Translation extends GP_Route_Main {
 			$this->errors[] = 'No translations were supplied.';
 		}
 
-		// hack, until we make clean_url() to allow [ and ]
-		$bulk['redirect_to'] = str_replace( array('[', ']'), array_map('urlencode', array('[', ']')), $bulk['redirect_to']);
+		$bulk['redirect_to'] = esc_url_raw( $bulk['redirect_to'] );
 		$this->redirect( $bulk['redirect_to'] );
 	}
 
