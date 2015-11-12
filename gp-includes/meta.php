5e55508940439332c05bbe7aeb5cdd35406736f3
Dominik Schilling <dominikschilling+git@gmail.com>
2015-11-12 14:49:05 +0100

    Remove unused and broken `gp_append_meta()` function.
    Fixes #145.

--- gp-includes/meta.php
+++ gp-includes/meta.php
@@ -110,8 +110,7 @@ function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
  *
  * @internal
  */
-function gp_delete_meta( $object_id = 0, $meta_key, $meta_value, $type, $global = false )
-{
+function gp_delete_meta( $object_id = 0, $meta_key, $meta_value, $type, $global = false ) {
 	global $wpdb;
 	if ( !is_numeric( $object_id ) || empty( $object_id ) && !$global ) {
 		return false;
@@ -155,70 +154,3 @@ function gp_delete_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 	wp_cache_delete( $cache_object_id, $object_type );
 	return true;
 }
-
-/**
- * Adds an objects meta data to the object
- *
- * This is the only function that should add to user / topic - NOT gpdb::prepared
- *
- * @internal
- */
-function gp_append_meta( $object, $type )
-{
-	global $wpdb;
-	if ( 'user' === $type ) {
-		return $object;
-	}
-
-	if ( is_array( $object ) && $object ) {
-		$trans = array();
-		foreach ( array_keys( $object ) as $i ) {
-			$trans[$object[$i]->$object_id_column] =& $object[$i];
-		}
-		$ids = join( ',', array_map( 'intval', array_keys( $trans ) ) );
-		if ( $metas = $wpdb->get_results( "SELECT `object_id`, `meta_key`, `meta_value` FROM `$wpdb->gp_meta` WHERE `object_type` = '$object_type' AND `object_id` IN ($ids) /* gp_append_meta */" ) ) {
-			usort( $metas, '_gp_append_meta_sort' );
-			foreach ( $metas as $meta ) {
-				$trans[$meta->object_id]->{$meta->meta_key} = maybe_unserialize( $meta->meta_value );
-				if ( strpos($meta->meta_key, $wpdb->prefix) === 0 ) {
-					$trans[$meta->object_id]->{substr($meta->meta_key, strlen($wpdb->prefix))} = maybe_unserialize( $meta->meta_value );
-				}
-			}
-		}
-		foreach ( array_keys( $trans ) as $i ) {
-			wp_cache_add( $i, $trans[$i], $object_type );
-			if ( $slug ) {
-				wp_cache_add( $trans[$i]->$slug, $i, 'gp_' . $slug );
-			}
-		}
-		return $object;
-	} elseif ( $object ) {
-		if ( $metas = $wpdb->get_results( $wpdb->prepare( "SELECT `meta_key`, `meta_value` FROM `$wpdb->gp_meta` WHERE `object_type` = '$object_type' AND `object_id` = %d /* gp_append_meta */", $object->$object_id_column ) ) ) {
-			usort( $metas, '_gp_append_meta_sort' );
-			foreach ( $metas as $meta ) {
-				$object->{$meta->meta_key} = maybe_unserialize( $meta->meta_value );
-				if ( strpos( $meta->meta_key, $wpdb->prefix ) === 0 ) {
-					$object->{substr( $meta->meta_key, strlen( $wpdb->prefix ) )} = $object->{$meta->meta_key};
-				}
-			}
-		}
-		if ( $object->$object_id_column ) {
-			wp_cache_set( $object->$object_id_column, $object, $object_type );
-			if ( $slug ) {
-				wp_cache_add( $object->$slug, $object->$object_id_column, 'gp_' . $slug );
-			}
-		}
-		return $object;
-	}
-}
-
-/**
- * Sorts meta keys by length to ensure $appended_object->{$wpdb->prefix} key overwrites $appended_object->key as desired
- *
- * @internal
- */
-function _gp_append_meta_sort( $a, $b )
-{
-	return strlen( $a->meta_key ) - strlen( $b->meta_key );
-}
-
