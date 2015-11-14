a35b04a1645d40093c35a326b26e659ddf053296
Greg Ross <greg@toolstack.com>
2015-11-13 19:46:15 -0500

    Moved function from default-filters.php and cleaned up merge issues.

--- gp-includes/misc.php
+++ gp-includes/misc.php
@@ -326,3 +326,25 @@ function gp_gmt_strtotime( $string ) {
 
 	return $time;
 }
+
+function gp_wp_profile( $user ) {
+	// If the user cannot edit their profile, then don't show the settings.
+	if ( !current_user_can( 'edit_user', $user_id ) ) { return false; }
+?>
+	<h3 id="gp-profile"><?php _e('GlotPress Profile'); ?></h3>
+<?php		
+	
+	include( GP_PATH . './gp-templates/profile-edit.php' );
+}
+
+function gp_wp_profile_update( $user_id ) {
+	// If the user cannot edit their profile, then don't save the settings
+	if ( !current_user_can( 'edit_user', $user_id ) ) { return false; }
+	
+	$gp_route_profile = new GP_Route_Profile;
+	
+	$gp_route_profile->profile_post( $user_id );
+	
+	return true;
+}
+
--- gp-templates/profile.php
+++ gp-templates/profile.php
@@ -2,9 +2,12 @@
 gp_title( __( 'Profile &lt; GlotPress', 'glotpress' ) );
 gp_breadcrumb( array( __( 'Profile', 'glotpress' ) ) );
 gp_tmpl_header();
+
 $per_page = get_user_option( 'gp_per_page' );
-if ( 0 == $per_page )
+if ( 0 == $per_page ) {
+	$per_page = 15;
 }
+
 $default_sort = get_user_option( 'gp_default_sort' );
 if ( ! is_array( $default_sort ) ) {
 	$default_sort = array(
@@ -16,22 +19,10 @@ if ( ! is_array( $default_sort ) ) {
 <h2><?php _e( 'Profile', 'glotpress' ); ?></h2>
 <form action="" method="post">
 <?php 
-			<th><label for="per_page"><?php _e( "Number of items per page:" ); ?></label></th>
 include_once( dirname( __FILE__ ) . '/profile-edit.php' );
 ?>
-			<th><label for="default_sort[by]"><?php _e("Default Sort By:") ?></label></th>
-			'original_date_added' => __('Date added (original)'),
-			'translation_date_added' => __('Date added (translation)'),
-			'original' => __('Original string'),
-			'translation' => __('Translation'),
-			'priority' => __('Priority'),
-			'references' => __('Filename in source'),
-			'random' => __('Random'),
-			<th><label for="default_sort[how]"><?php _e("Default Sort Order:") ?></label></th>
-					'asc' => __('Ascending'),
-					'desc' => __('Descending'),
 	<br>
 	<input type="submit" name="submit" value="<?php esc_attr_e( 'Change Settings', 'glotpress' ); ?>">
 </form>
-<?php
-gp_tmpl_footer();
+
+<?php gp_tmpl_footer();
