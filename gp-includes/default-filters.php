01bd9ae5874f42f6ae1196716f419f575f9f6ba1
Dominik Schilling <dominikschilling+git@gmail.com>
2015-11-22 14:37:05 +0100

    Codestyling: Fix some code indentation issues.
    Fixes #158.

--- gp-includes/default-filters.php
+++ gp-includes/default-filters.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * Filters and actions assigned by default.
+ * Sets up the default filters and actions for most
+ * of the GlotPress hooks.
+ *
+ * If you need to remove a default hook, this file will
+ * give you the priority for which to use to remove the
+ * hook.
+ *
+ * @package GlotPress
  */
 
 // Actions
