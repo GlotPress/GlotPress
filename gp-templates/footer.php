b538e72498f5881638493664dade48e3317cf7e9
Dominik Schilling <dominikschilling+git@gmail.com>
2016-01-07 23:07:04 +0100

    Template: Remove title attribute from the powered by link.

--- gp-templates/footer.php
+++ gp-templates/footer.php
@@ -1,6 +1,6 @@
 	</div><!-- .gp-content -->
 	<p id="gp-footer" class="secondary">
-		<?php printf( __( 'Proudly powered by %s.', 'glotpress' ), '<a href="http://glotpress.org/" title="' .  esc_attr__( 'Found in translation', 'glotpress' ) . '">GlotPress</a>' ); ?>
+		<?php printf( __( 'Proudly powered by %s.', 'glotpress' ), '<a href="http://glotpress.org/">GlotPress</a>' ); ?>
 		<?php gp_footer(); ?>
 	</p>
 	</body>
