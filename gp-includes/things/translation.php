b240cd3b0411c3b4d0b4f3a5e8d660098fefdcd6
Greg Ross <greg@toolstack.com>
2016-02-04 15:04:57 -0500

    Define class variables for each field in the thing.

--- gp-includes/things/translation.php
+++ gp-includes/things/translation.php
@@ -6,6 +6,20 @@ class GP_Translation extends GP_Thing {
 	var $field_names = array( 'id', 'original_id', 'translation_set_id', 'translation_0', 'translation_1', 'translation_2', 'translation_3', 'translation_4', 'translation_5','user_id', 'status', 'date_added', 'date_modified', 'warnings' );
 	var $int_fields = array( 'id', 'original_id', 'translation_set_id', 'user_id' );
 	var $non_updatable_attributes = array( 'id', );
+	var $id;
+	var $original_id;
+	var $translation_set_id;
+	var $translation_0;
+	var $translation_1;
+	var $translation_2;
+	var $translation_3;
+	var $translation_4;
+	var $translation_5;
+	var $user_id;
+	var $status;
+	var $date_added;
+	var $date_modified;
+	var $warningsvar;
 
 	static $statuses = array( 'current', 'waiting', 'rejected', 'fuzzy', 'old', );
 	static $number_of_plural_translations = 6;
