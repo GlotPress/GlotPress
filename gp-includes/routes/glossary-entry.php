2971170a0b378c0c45a47a17879795367d7e951c
Greg Ross <greg@toolstack.com>
2016-02-04 16:00:47 -0500

    Assign the return value of gp_post() to a variable before passing it to array_shift() in glossary_entries_post() as array_shift() expects a pass by reference and a function return value cannot be passed by reference.
    When checking for errors its an array so don't just see if it exists, instead check to see if its not empty.

--- gp-includes/routes/glossary-entry.php
+++ gp-includes/routes/glossary-entry.php
@@ -78,7 +78,8 @@ class GP_Route_Glossary_Entry extends GP_Route_Main {
 	}
 
 	public function glossary_entries_post( $project_path, $locale_slug, $translation_set_slug ) {
-		$ge              = array_shift( gp_post('glossary_entry') );
+		$ge_post 		= gp_post('glossary_entry');
+		$ge             = array_shift( $ge_post );
 		$glossary_entry  = GP::$glossary_entry->get( absint( $ge['glossary_entry_id'] ) );
 
 		if ( ! $glossary_entry ){
@@ -148,7 +149,7 @@ class GP_Route_Glossary_Entry extends GP_Route_Main {
 			$this->errors[] = __( 'Error in deleting glossary entry!', 'glotpress' );
 		}
 
-		if ( $this->errors ) {
+		if ( ! empty( $this->errors ) ) {
 			$error_output = '<ul>';
 			foreach ( $this->errors as $error ) {
 				$error_output .= '<li>' . $error . '</li>';
