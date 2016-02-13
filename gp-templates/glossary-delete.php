96499df67135b8343873c7344de92445e3aa8c0a
Greg Ross <greg@toolstack.com>
2016-02-13 09:48:54 -0500

    Add glossary delete template file.

---
+++ gp-templates/glossary-delete.php
@@ -0,0 +1,26 @@
+<?php
+gp_title( __( 'Delete Glossary &lt; GlotPress', 'glotpress' ) );
+gp_breadcrumb( array(
+	gp_project_links_from_root( $project ),
+	gp_link_get( gp_url_project_locale( $project->path, $locale->slug, $translation_set->slug ), $translation_set->name ),
+	gp_link_get( gp_url_project_locale( $project->path, $locale->slug, $translation_set->slug ) . '/glossary', __( 'Glossary', 'glotpress' ) ),
+	__( 'delete', 'glotpress' )
+) );
+gp_tmpl_header();
+?>
+
+<h2><?php _e( 'Delete Glossary', 'glotpress' ); ?></h2>
+
+<form action="" method="post">
+	<p>
+		<?php _e( 'Note this will delete all entries associated with this glossary!', 'glotpress' ); ?>
+		<input type="hidden" name="glossary" value="<?php echo $glossary->id; ?>" id="glossary" />
+	</p>
+
+	<p>
+		<input type="submit" name="submit" value="<?php esc_attr_e( 'Delete', 'glotpress' ); ?>" id="submit" />
+		<span class="or-cancel"><?php _e( 'or', 'glotpress' ); ?> <a href="javascript:history.back();"><?php _e( 'Cancel', 'glotpress' ); ?></a></span>
+	</p>
+</form>
+
+<?php gp_tmpl_footer();
