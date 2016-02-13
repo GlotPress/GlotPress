69be5e0868ce628754a8683ab765de6fbd3706e1
Greg Ross <greg@toolstack.com>
2016-02-13 09:51:07 -0500

    Updated incorrect variable name.

--- gp-includes/things/glossary.php
+++ gp-includes/things/glossary.php
@@ -106,7 +106,7 @@ class GP_Glossary extends GP_Thing {
 	}
 
 	public function delete() {
-		$glossary_entries = GP::$glossary_entries->find_many( array( 'glossary_id', $this->id ) );
+		$glossary_entries = GP::$glossary_entry->find_many( array( 'glossary_id', $this->id ) );
 		
 		if ( ! empty( $glossary_entries ) ) {
 			foreach ( $glossary_entries as $entry ) {
