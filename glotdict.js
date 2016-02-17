f56ae5323c3a48399eab2dc1ddf674b882474278
Daniele Scasciafratte <mte90net@gmail.com>
2016-02-17 22:16:51 +0100

    working on #1

--- glotdict.js
+++ glotdict.js
@@ -1,3 +1,16 @@
-/*
-TODO
-*/
+jQuery(function() {
+   jQuery('table.translations')
+        .on('click', 'a.edit', gd_add_terms)
+        .on('dblclick', 'tr td', gd_add_terms);
+        
+    function gd_add_terms(e) {
+        e.preventDefault();
+	var row_id = jQuery(this).closest('tr').attr('row');
+	var editor = jQuery('#editor-' + row_id);
+	if (!editor.length) {
+		return;
+	}
+	var editor_original = jQuery('#editor-' + row_id + ' .original');
+	console.log(editor_original);
+    }
+});
