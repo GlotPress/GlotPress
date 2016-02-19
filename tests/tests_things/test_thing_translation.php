58c9b4bd013b89f5f10b513e73ac44356619e8fe
Dominik Schilling <dominikschilling+git@gmail.com>
2016-02-19 16:31:08 +0100

    Tests: Extend propagation test to check if the translation is by the same user.
    Fixes #287.

--- tests/tests_things/test_thing_translation.php
+++ tests/tests_things/test_thing_translation.php
@@ -244,7 +244,7 @@ class GP_Test_Thing_Translation extends GP_UnitTestCase {
 		$original1 = $this->factory->original->create( array( 'project_id' => $set1->project_id, 'status' => '+active', 'singular' => 'baba' ) );
 		$original2 = $this->factory->original->create( array( 'project_id' => $set2->project_id, 'status' => '+active', 'singular' => 'baba' ) );
 
-		$translation1 = $this->factory->translation->create( array( 'translation_set_id' => $set1->id, 'original_id' => $original1->id, 'status' => 'current' ) );
+		$translation1 = $this->factory->translation->create( array( 'translation_set_id' => $set1->id, 'original_id' => $original1->id, 'user_id' => $user, 'status' => 'current' ) );
 
 		// Add the same translation as waiting to another set.
 		$translation_waiting = $translation1->fields();
@@ -260,5 +260,7 @@ class GP_Test_Thing_Translation extends GP_UnitTestCase {
 
 		$set2_current_translations = GP::$translation->for_export( $project2, $set2, array( 'status' => 'waiting' ) );
 		$this->assertEquals( 1, count( $set2_current_translations ) );
+
+		$this->assertEquals( $set2_current_translations[0]->user_login, wp_get_current_user()->user_login );
 	}
 }
