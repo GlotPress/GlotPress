6f89b4d91530d807606c1f4c46d2ba1f63f55e25
Mte90 <mte90smanettator@gmail.com>
2016-02-20 18:28:24 +0100

    working prototype!

--- glotdict.js
+++ glotdict.js
@@ -2,6 +2,7 @@ jQuery( document ).ready(function() {
     jQuery('table.translations')
 	.on('click', 'a.edit', gd_add_terms)
 	.on('dblclick', 'tr td', gd_add_terms);
+    //gd_add_terms()
 	
     function gd_add_terms(e) {
 	e.preventDefault();
@@ -10,29 +11,38 @@ jQuery( document ).ready(function() {
 	if (!editor.length) {
 		return;
 	}
-	//chrome.extension.getURL("images/myimage.png");
-	add_term('saved', '#editor-' + row_id + ' .original');
-        jQuery('.glossary-word-glotdict').css({'cursor': 'help','border-bottom':'1px dashed'});
-	editor.tooltip({
-		items: '.glossary-word-glotdict',
-		content: function(){
-			var content = jQuery('<ul>');
-			jQuery.each( jQuery(this).data('translations'), function( i, e ){
-				var def = jQuery('<li>');
-				def.append( jQuery('<span>', {text: e.pos }).addClass('pos') );
-				def.append( jQuery('<span>', {text: e.translation}).addClass('translation') );
-				def.append( jQuery('<span>', {text: e.comment}).addClass('comment') );
-				content.append(def);
-			});
-			return content;
-		}
-	});
+	jQuery.ajax({
+            url: glotdict_path + "/it_IT.json",
+            dataType: "json"
+        }).done(function( data ) {
+            jQuery.each(data, function(i, item) {
+                console.log(item)
+                add_term(i, '#editor-' + row_id + ' .original', item.translation, item.pos, item.comment);
+                jQuery('#editor-' + row_id + ' .original .glossary-word-glotdict').css({'cursor': 'help','border-bottom':'1px dashed'});
+                editor.tooltip({
+                        items: '#editor-' + row_id + ' .original .glossary-word-glotdict',
+                        content: function(){
+                                var content = jQuery('<ul>');
+                                jQuery.each( jQuery(this).data('translations'), function( i, e ){
+                                        var def = jQuery('<li>');
+                                        def.append( jQuery('<span>', {text: e.pos }).addClass('pos') );
+                                        def.append( jQuery('<span>', {text: e.translation}).addClass('translation') );
+                                        def.append( jQuery('<span>', {text: e.comment}).addClass('comment') );
+                                        content.append(def);
+                                });
+                                return content;
+                        }
+                });
+            });
+        }).fail(function() {
+            console.log( "error on loading the manifest" );
+        });
     }
     
-    function add_term(word, element) {
+    function add_term(word, element, translation, pos, comment) {
 	element = document.querySelector(element);
 	var rgxp = new RegExp(word, 'g');
-	var repl = "<span class=\"glossary-word-glotdict\" data-translations='[{\"translation\":\"salvato\",\"pos\":\"noun\",\"comment\":\"\"}]'>" + word + '</span>';
+	var repl = "<span class=\"glossary-word-glotdict\" data-translations='[{\"translation\":\"" + translation + "\",\"pos\":\"" + pos + "\",\"comment\":\"" + comment + "\"}]'>" + word + '</span>';
 	element.innerHTML = element.innerHTML.replace(rgxp, repl);
     }
 });
--- init.js
+++ init.js
@@ -1,3 +1,9 @@
+//Set the path of dictionaries
+var s = document.createElement('script');
+s.textContent = 'var glotdict_path = "' + chrome.extension.getURL("dictionaries") + '"';
+(document.head||document.documentElement).appendChild(s);
+
+//Load GlotDict
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('glotdict.js');
 s.type = 'text/javascript';
@@ -5,3 +11,4 @@ s.onload = function() {
     this.parentNode.removeChild(this);
 };
 (document.head || document.documentElement).appendChild(s); 
+
