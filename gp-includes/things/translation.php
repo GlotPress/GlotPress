9b3367e496ed129bec340af6ae17606bd733bcd6
Greg Ross <greg@toolstack.com>
2016-02-28 13:43:07 -0500

    Remove unneeded method doc block as the thing object no longer uses the magic methods.

--- gp-includes/things/translation.php
+++ gp-includes/things/translation.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @method object|array many_no_map( string $sql )
- * @method object|array find_no_map( array $options )
- * @method object|array find_many_no_map( string|array $sql )
- * @method object|array value_no_map( string|array $sql )
- */
 class GP_Translation extends GP_Thing {
 
 	public $per_page = 15;
