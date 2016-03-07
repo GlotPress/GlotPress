004b2a26496539fd2800db22353ce692c4f7d096
Greg Ross <greg@toolstack.com>
2016-03-07 15:50:54 -0500

    Remove unused user/option code in meta functions.
    Also add doc blocks and clean up formating.

--- gp-includes/meta.php
+++ gp-includes/meta.php
@@ -9,13 +9,24 @@
 
 /* Internal */
 
+/**
+ * Sanitizes a key name to be used to store meta data in to the database.
+ *
+ * @since 1.0.0
+ *
+ * @param string $meta_key Metadata key.
+ *
+ * @return string
+ */
 function gp_sanitize_meta_key( $key ) {
 	return preg_replace( '|[^a-z0-9_]|i', '', $key );
 }
 
 
 /**
- * Retrieves and returns a meta value from the database
+ * Retrieves and returns a meta value from the database.
+ *
+ * @since 1.0.0
  *
  * @param string      $object_type The object type.
  * @param int         $object_id   ID of the object metadata is for.
@@ -61,7 +72,17 @@ function gp_get_meta( $object_type, $object_id, $meta_key = null ) {
 /**
  * Adds and updates meta data in the database
  *
+ * @since 1.0.0
+ *
  * @internal
+ *
+ * @param int     $object_id   ID of the object metadata is for.
+ * @param string  $meta_key    Metadata key.
+ * @param mixed   $meta_value  The value to store.
+ * @param string  $object_type The object type.
+ * @param bool    $global      Overrides the requirement of $object_id to be a number OR not empty.
+ *
+ * @return bool|null
  */
 function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global = false ) {
 	global $wpdb;
@@ -69,18 +90,9 @@ function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 	if ( !is_numeric( $object_id ) || empty( $object_id ) && !$global ) {
 		return false;
 	}
+	
 	$cache_object_id = $object_id = (int) $object_id;
-	switch ( $type ) {
-		case 'option':
-			$object_type = 'gp_option';
-			break;
-		case 'user' :
-			return update_user_meta( $object_id, $meta_key, $meta_value );
-		default :
-			$object_type = $type;
-			break;
-	}
-
+	$object_type = $type;
 	$meta_key = gp_sanitize_meta_key( $meta_key );
 
 	$meta_tuple = compact( 'object_type', 'object_id', 'meta_key', 'meta_value', 'type' );
@@ -100,16 +112,13 @@ function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 	$meta_value = maybe_unserialize( $meta_value );
 
 	$cur = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM `$wpdb->gp_meta` WHERE `object_type` = %s AND `object_id` = %d AND `meta_key` = %s", $object_type, $object_id, $meta_key ) );
+	
 	if ( !$cur ) {
 		$wpdb->insert( $wpdb->gp_meta, array( 'object_type' => $object_type, 'object_id' => $object_id, 'meta_key' => $meta_key, 'meta_value' => $_meta_value ) );
 	} elseif ( $cur->meta_value != $meta_value ) {
 		$wpdb->update( $wpdb->gp_meta, array( 'meta_value' => $_meta_value), array( 'object_type' => $object_type, 'object_id' => $object_id, 'meta_key' => $meta_key ) );
 	}
 
-	if ( $object_type === 'gp_option' ) {
-		$cache_object_id = $meta_key;
-		wp_cache_delete( $cache_object_id, 'gp_option_not_set' );
-	}
 	wp_cache_delete( $cache_object_id, $object_type );
 
 	if ( !$cur ) {
@@ -118,27 +127,29 @@ function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 }
 
 /**
- * Deletes meta data from the database
+ * Deletes meta data from the database.
+ *
+ * @since 1.0.0
  *
  * @internal
+ *
+ * @param int     $object_id   ID of the object metadata is for.
+ * @param string  $meta_key    Metadata key.
+ * @param mixed   $meta_value  The value to store.
+ * @param string  $type        The object type.
+ * @param bool    $global      Overrides the requirement of $object_id to be a number OR not empty.
+ *
+ * @return bool
  */
 function gp_delete_meta( $object_id = 0, $meta_key, $meta_value, $type, $global = false ) {
 	global $wpdb;
+
 	if ( !is_numeric( $object_id ) || empty( $object_id ) && !$global ) {
 		return false;
 	}
+	
 	$cache_object_id = $object_id = (int) $object_id;
-	switch ( $type ) {
-		case 'option':
-			$object_type = 'gp_option';
-			break;
-		case 'user':
-			return delete_user_meta( $object_id, $meta_key, $meta_value );
-		default:
-			$object_type = $type;
-			break;
-	}
-
+	$object_type = $type;
 	$meta_key = gp_sanitize_meta_key( $meta_key );
 
 	$meta_tuple = compact( 'object_type', 'object_id', 'meta_key', 'meta_value', 'type' );
@@ -167,10 +178,7 @@ function gp_delete_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 
 	$wpdb->query( $wpdb->prepare( "DELETE FROM `$wpdb->gp_meta` WHERE `meta_id` = %d", $meta_id ) );
 
-	if ( $object_type == 'gp_option' ) {
-		$cache_object_id = $meta_key;
-		wp_cache_delete( $cache_object_id, 'gp_option_not_set' );
-	}
 	wp_cache_delete( $cache_object_id, $object_type );
+	
 	return true;
 }
