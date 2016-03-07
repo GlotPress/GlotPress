dd8023a51f08112046f4ef62a93181ce23329556
Dominik Schilling <dominikschilling+git@gmail.com>
2016-03-07 20:56:01 +0100

    Update plugin header to use a docblock.

--- glotpress.php
+++ glotpress.php
@@ -1,33 +1,30 @@
 <?php
 /**
+ * Plugin Name: GlotPress
+ * Plugin URI: https://wordpress.org/plugins/glotpress/
+ * Description: GlotPress is a tool to help translators collaborate.
+ * Version: 1.1.0-alpha
+ * Author: the GlotPress team
+ * Author URI: http://glotpress.org
+ * License: GPLv2 or later
+ * Text Domain: glotpress
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  * @package GlotPress
  */
-/*
-Plugin Name: GlotPress
-Plugin URI: https://wordpress.org/plugins/glotpress/
-Description: GlotPress is a tool to help translators collaborate.
-Version: 1.1.0-alpha
-Author: the GlotPress team
-Author URI: http://glotpress.org
-License: GPLv2 or later
-Text Domain: glotpress
-*/
-
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
 
 define( 'GP_VERSION', '1.1.0-alpha' );
 define( 'GP_DB_VERSION', '940' );
