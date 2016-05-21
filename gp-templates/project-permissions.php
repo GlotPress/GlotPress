0a776b7375902e02353537f8b664c39bdfc0be3c
Dominik Schilling <dominikschilling+git@gmail.com>
2016-05-21 13:26:59 +0200

    Template: Remove 'tablesorter' class from permissions table.

--- gp-templates/project-permissions.php
+++ gp-templates/project-permissions.php
@@ -15,7 +15,7 @@ gp_tmpl_header();
 <h4 id="validators"><?php _e( 'Validators for this project', 'glotpress' ); ?></h4>
 	<?php endif; ?>
 
-	<table class="permissions tablesorter">
+	<table class="permissions">
 		<thead>
 			<tr>
 				<th><?php _e( 'User', 'glotpress' ); ?></th>
@@ -39,7 +39,7 @@ gp_tmpl_header();
 	<?php endif; ?>
 	<?php  if ( $parent_permissions ) : ?>
 <h4 id="validators"><?php _e( 'Validators for parent projects', 'glotpress' ); ?></h4>
-	<table class="permissions tablesorter">
+	<table class="permissions">
 		<thead>
 			<tr>
 				<th><?php _e( 'User', 'glotpress' ); ?></th>
