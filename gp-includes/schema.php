a1bd3a9753bca6b66ef4f5dc3ae0e876695a7d71
Dominik Schilling <dominikschilling+git@gmail.com>
2016-06-07 18:44:40 +0200

    Schema: Use lowercase field types.
    Avoids unnecessary queries by `dbDelta()` which compares the schema against `DESCRIBE`.

--- gp-includes/schema.php
+++ gp-includes/schema.php
@@ -29,20 +29,20 @@ function gp_schema_get() {
 	 *  - Status can be: new, approved, unaproved, current, spam or whatever you'd like
 	 */
 	$gp_schema['translations'] = "CREATE TABLE $wpdb->gp_translations (
-		id INT(10) NOT NULL auto_increment,
-		original_id INT(10) DEFAULT NULL,
-		translation_set_id INT(10) DEFAULT NULL,
-		translation_0 TEXT NOT NULL,
-		translation_1 TEXT DEFAULT NULL,
-		translation_2 TEXT DEFAULT NULL,
-		translation_3 TEXT DEFAULT NULL,
-		translation_4 TEXT DEFAULT NULL,
-		translation_5 TEXT DEFAULT NULL,
-		user_id INT(10) DEFAULT NULL,
-		status VARCHAR(20) NOT NULL default 'waiting',
-		date_added DATETIME DEFAULT NULL,
-		date_modified DATETIME DEFAULT NULL,
-		warnings TEXT DEFAULT NULL,
+		id int(10) NOT NULL auto_increment,
+		original_id int(10) DEFAULT NULL,
+		translation_set_id int(10) DEFAULT NULL,
+		translation_0 text NOT NULL,
+		translation_1 text DEFAULT NULL,
+		translation_2 text DEFAULT NULL,
+		translation_3 text DEFAULT NULL,
+		translation_4 text DEFAULT NULL,
+		translation_5 text DEFAULT NULL,
+		user_id int(10) DEFAULT NULL,
+		status varchar(20) NOT NULL default 'waiting',
+		date_added datetime DEFAULT NULL,
+		date_modified datetime DEFAULT NULL,
+		warnings text DEFAULT NULL,
 		PRIMARY KEY  (id),
 		KEY original_id (original_id),
 		KEY user_id (user_id),
@@ -59,11 +59,11 @@ function gp_schema_get() {
 	 * Most projects will have only one translation set per locale.
 	 */
 	$gp_schema['translation_sets'] = "CREATE TABLE $wpdb->gp_translation_sets (
-		id INT(10) NOT NULL auto_increment,
-		name VARCHAR(255) NOT NULL,
-		slug VARCHAR(255) NOT NULL,
-		project_id INT(10) DEFAULT NULL,
-		locale VARCHAR(10) DEFAULT NULL,
+		id int(10) NOT NULL auto_increment,
+		name varchar(255) NOT NULL,
+		slug varchar(255) NOT NULL,
+		project_id int(10) DEFAULT NULL,
+		locale varchar(10) DEFAULT NULL,
 		PRIMARY KEY  (id),
 		UNIQUE KEY project_id_slug_locale (project_id,slug(171),locale),
 		KEY locale_slug (locale,slug(181))
@@ -82,16 +82,16 @@ function gp_schema_get() {
 	 *
 	 */
 	$gp_schema['originals'] = "CREATE TABLE $wpdb->gp_originals (
-		id INT(10) NOT NULL auto_increment,
-		project_id INT(10) DEFAULT NULL,
-		context VARCHAR(255) DEFAULT NULL,
-		singular TEXT NOT NULL,
-		plural TEXT DEFAULT NULL,
-		`references` TEXT DEFAULT NULL,
-		comment TEXT DEFAULT NULL,
-		status VARCHAR(255) NOT NULL DEFAULT '+active',
-		priority TINYINT NOT NULL DEFAULT 0,
-		date_added DATETIME DEFAULT NULL,
+		id int(10) NOT NULL auto_increment,
+		project_id int(10) DEFAULT NULL,
+		context varchar(255) DEFAULT NULL,
+		singular text NOT NULL,
+		plural text DEFAULT NULL,
+		`references` text DEFAULT NULL,
+		comment text DEFAULT NULL,
+		status varchar(255) NOT NULL DEFAULT '+active',
+		priority tinyint(4) NOT NULL DEFAULT 0,
+		date_added datetime DEFAULT NULL,
 		PRIMARY KEY  (id),
 		KEY project_id_status (project_id,status),
 		KEY singular_plural_context (singular(63),plural(63),context(63)),
@@ -102,14 +102,14 @@ function gp_schema_get() {
 	 * Glossary Entries
 	 */
 	$gp_schema['glossary_entries'] = "CREATE TABLE $wpdb->gp_glossary_entries (
-		id INT(10) unsigned NOT NULL auto_increment,
-		glossary_id INT(10) unsigned NOT NULL,
-		term VARCHAR(255) NOT NULL,
-		part_of_speech VARCHAR(255) DEFAULT NULL,
-		comment TEXT DEFAULT NULL,
-		translation VARCHAR(255) DEFAULT NULL,
-		date_modified DATETIME NOT NULL,
-		last_edited_by BIGINT(20) NOT NULL,
+		id int(10) unsigned NOT NULL auto_increment,
+		glossary_id int(10) unsigned NOT NULL,
+		term varchar(255) NOT NULL,
+		part_of_speech varchar(255) DEFAULT NULL,
+		comment text DEFAULT NULL,
+		translation varchar(255) DEFAULT NULL,
+		date_modified datetime NOT NULL,
+		last_edited_by bigint(20) NOT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;";
 
@@ -117,9 +117,9 @@ function gp_schema_get() {
 	 * Glossaries
 	 */
 	$gp_schema['glossaries'] = "CREATE TABLE $wpdb->gp_glossaries (
-		id INT(10) unsigned NOT NULL auto_increment,
-		translation_set_id INT(10)  NOT NULL,
-		description TEXT DEFAULT NULL,
+		id int(10) unsigned NOT NULL auto_increment,
+		translation_set_id int(10)  NOT NULL,
+		description text DEFAULT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;";
 
@@ -129,14 +129,14 @@ function gp_schema_get() {
 	 * - The path is the combination of the slugs of all its parents, separated by /
 	 */
 	$gp_schema['projects'] = "CREATE TABLE $wpdb->gp_projects (
-		id INT(10) NOT NULL auto_increment,
-		name VARCHAR(255) NOT NULL,
-		slug VARCHAR(255) NOT NULL,
-		path VARCHAR(255) NOT NULL,
-		description TEXT NOT NULL,
-		parent_project_id INT(10) DEFAULT NULL,
-		source_url_template VARCHAR(255) DEFAULT '',
-		active TINYINT DEFAULT 0,
+		id int(10) NOT NULL auto_increment,
+		name varchar(255) NOT NULL,
+		slug varchar(255) NOT NULL,
+		path varchar(255) NOT NULL,
+		description text NOT NULL,
+		parent_project_id int(10) DEFAULT NULL,
+		source_url_template varchar(255) DEFAULT '',
+		active tinyint(4) DEFAULT 0,
 		PRIMARY KEY  (id),
 		KEY path (path),
 		KEY parent_project_id (parent_project_id)
@@ -160,11 +160,11 @@ function gp_schema_get() {
 	 * Permissions
 	 */
 	$gp_schema['permissions'] = "CREATE TABLE $wpdb->gp_permissions (
-		id INT(10) NOT NULL AUTO_INCREMENT,
-		user_id INT(10) DEFAULT NULL,
-		action VARCHAR(255) DEFAULT NULL,
-		object_type VARCHAR(255) DEFAULT NULL,
-		object_id VARCHAR(255) DEFAULT NULL,
+		id int(10) NOT NULL AUTO_INCREMENT,
+		user_id int(10) DEFAULT NULL,
+		action varchar(255) DEFAULT NULL,
+		object_type varchar(255) DEFAULT NULL,
+		object_id varchar(255) DEFAULT NULL,
 		PRIMARY KEY  (id),
 		KEY user_id_action (user_id,action)
 	) $charset_collate;";
