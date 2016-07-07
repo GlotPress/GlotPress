5869acf6c8c7b44600fe2ac4b9b290920a54d260
Greg Ross <greg@toolstack.com>
2016-07-07 07:39:46 -0400

    Fix how GP loads javascript to only load the required scripts per page.

--- gp-includes/assets-loader.php
+++ gp-includes/assets-loader.php
@@ -17,25 +17,6 @@ function gp_styles_default( &$styles ) {
 add_action( 'wp_default_styles', 'gp_styles_default' );
 
 /**
- * Register the GlotPress scripts
- *
- * @param WP_Scripts $scripts
- */
-function gp_scripts_default( &$scripts ) {
-	$url = gp_plugin_url( 'assets/js' );
-
-	$scripts->add( 'tablesorter', $url . '/jquery.tablesorter.min.js', array( 'jquery' ), '1.10.4' );
-
-	$scripts->add( 'gp-common', $url . '/common.js', array( 'jquery' ), '20150430' );
-	$scripts->add( 'gp-editor', $url . '/editor.js', array( 'gp-common', 'jquery-ui-tooltip' ), '20160329' );
-	$scripts->add( 'gp-glossary', $url . '/glossary.js', array( 'gp-common' ), '20160329' );
-	$scripts->add( 'gp-translations-page', $url . '/translations-page.js', array( 'gp-common' ), '20150430' );
-	$scripts->add( 'mass-create-sets-page', $url . '/mass-create-sets-page.js', array( 'gp-common' ), '20150430' );
-}
-
-add_action( 'wp_default_scripts', 'gp_scripts_default' );
-
-/**
  * Here we abstract WordPress core's enqueuing functions because...
  * 1. We don't want to print scripts and styles that are meant for the WordPress theme
  * 2. GlotPress enqueues scripts and styles from its template files and if we do that
@@ -43,42 +24,36 @@ add_action( 'wp_default_scripts', 'gp_scripts_default' );
  *    those functions should only be called inside of wp_enqueue_scripts()
  */
 
-function gp_enqueue_scripts() {
-	global $gp_enqueued_styles, $gp_enqueued_scripts;
+function gp_register_scripts() {
+	$url = gp_plugin_url( 'assets/js' );
 
-	if ( ! empty( $gp_enqueued_scripts ) ) {
-		foreach ( $gp_enqueued_scripts as $handle ) {
-			wp_enqueue_script( $handle );
-		}
-	}
-
-	if ( ! empty( $gp_enqueued_styles ) ) {
-		foreach ( $gp_enqueued_styles as $handle ) {
-			wp_enqueue_style( $handle );
-		}
-	}
+	wp_register_script( 'tablesorter', $url . '/jquery.tablesorter.min.js', array( 'jquery' ), '1.10.4' );
+	wp_register_script( 'gp-common', $url . '/common.js', array( 'jquery' ), '20150430' );
+	wp_register_script( 'gp-editor', $url . '/editor.js', array( 'gp-common', 'jquery-ui-tooltip' ), '20160329' );
+	wp_register_script( 'gp-glossary', $url . '/glossary.js', array( 'gp-common' ), '20160329' );
+	wp_register_script( 'gp-translations-page', $url . '/translations-page.js', array( 'gp-common' ), '20150430' );
+	wp_register_script( 'mass-create-sets-page', $url . '/mass-create-sets-page.js', array( 'gp-common' ), '20150430' );
 }
 
-add_action( 'wp_enqueue_scripts', 'gp_enqueue_scripts' );
+add_action( 'init', 'gp_register_scripts' );
 
 function gp_enqueue_style( $handle ) {
 	global $gp_enqueued_styles;
 
 	$gp_enqueued_styles[] = $handle;
+	wp_enqueue_style( $handle );
 }
 
 function gp_enqueue_script( $handle ) {
-	global $gp_enqueued_scripts;
-
-	$gp_enqueued_scripts[] = $handle;
+	wp_enqueue_script( $handle );
 }
 
 function gp_print_styles() {
 	global $gp_enqueued_styles;
+
 	wp_print_styles( $gp_enqueued_styles );
 }
 
 function gp_print_scripts() {
-	global $gp_enqueued_scripts;
-	wp_print_scripts( $gp_enqueued_scripts );
+	wp_print_scripts();
 }
