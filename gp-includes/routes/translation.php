86275fb541edc24d282a83e83275343cbb6e6a7c
Greg Ross <greg@toolstack.com>
2016-07-21 12:46:27 -0400

    Fixed loop structures using number_of_plural_translations.

--- gp-includes/routes/translation.php
+++ gp-includes/routes/translation.php
@@ -259,7 +259,8 @@ class GP_Route_Translation extends GP_Route_Main {
 			$data['user_id'] = get_current_user_id();
 			$data['translation_set_id'] = $translation_set->id;
 
-			foreach( range( 0, GP::$translation->get_static( 'number_of_plural_translations' ) ) as $i ) {
+			// Reduce range by one since we're starting at 0, see GH#516.
+			foreach ( range( 0, GP::$translation->get_static( 'number_of_plural_translations' ) - 1 ) as $i ) {
 				if ( isset( $translations[$i] ) ) $data["translation_$i"] = $translations[$i];
 			}
 
