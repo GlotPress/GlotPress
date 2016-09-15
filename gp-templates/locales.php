2c440f43c61c2ce1c79f4253432d38cf1f9adb7e
Greg Ross <greg@toolstack.com>
2016-09-15 18:12:19 -0400

    Format the locales table to be as close to the final output as possible.
    This avoid most "flicker" in Chrome, however there is still a "bounce" as Chrome looks to be miscalculating the column width during the first render of the table and then correcting it afterwards.

--- gp-templates/locales.php
+++ gp-templates/locales.php
@@ -12,18 +12,29 @@ gp_tmpl_header();
 		<?php _e( 'Filter:', 'glotpress' );?><input id="locales-filter" type="text" placeholder="<?php esc_attr_e('search', 'glotpress'); ?>" />
 	</div>
 
-	<table class="tablesorter locales">
+	<table class="tablesorter locales tablesorter-glotpress">
 		<thead>
-		<tr>
-			<th class="header"><?php _e( 'Name (in English)', 'glotpress' );?></th>
-			<th class="header"><?php _e( 'Native name', 'glotpress' );?></th>
-			<th class="header"><?php _e( 'Language code', 'glotpress' );?></th>
+		<tr class="tablesorter-headerRow">
+			<th class="header tablesorter-header tablesorter-headerAsc"><?php _e( 'Name (in English)', 'glotpress' );?></th>
+			<th class="header tablesorter-header tablesorter-headerUnSorted"><?php _e( 'Native name', 'glotpress' );?></th>
+			<th class="header tablesorter-header tablesorter-headerUnSorted"><?php _e( 'Language code', 'glotpress' );?></th>
 
 		</tr>
 		</thead>
 		<tbody>
-		<?php foreach ( $locales as $locale ) : ?>
-			<tr>
+		<?php 
+			$i = 0;
+			
+			foreach ( $locales as $locale ) : 
+				if( $i % 2 ) {
+					$oe = 'even';
+				} else {
+					$oe = 'odd';
+				}
+				
+				$i++;
+?>
+			<tr class="<?php echo $oe; ?>">
 				<?php echo "<td>" . gp_link_get( gp_url_join( gp_url_current(), $locale->slug ), $locale->english_name ) . "</td>" ?>
 				<?php echo "<td>" . gp_link_get( gp_url_join( gp_url_current(), $locale->slug ), $locale->native_name ) . "</td>" ?>
 				<?php echo "<td>" . gp_link_get( gp_url_join( gp_url_current(), $locale->slug ), $locale->slug ) . "</td>" ?>
@@ -44,9 +55,7 @@ gp_tmpl_header();
 				},
 				widgets: ['zebra']
 			});
-
-			$('.locales').width($('.locales').width());
-
+			
 			$rows = $('.locales tbody').find('tr');
 			$('#locales-filter').bind("change keyup input",function() {
 				var words = this.value.toLowerCase().split(' ');
