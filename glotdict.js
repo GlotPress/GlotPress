09928424aa136de8f3e0adfc9589e11aebf7c911
Daniele Scasciafratte <mte90net@gmail.com>
2016-09-25 12:34:25 +0200

    fix #48

--- glotdict.js
+++ glotdict.js
@@ -45,7 +45,7 @@ jQuery(document).ready(function () {
 	if (item !== '') {
 	  word = word.replace(/\)/g, "\\)");
 	  word = word.replace(/\(/g, "\\(");
-	  var rgxp = new RegExp('\\b[^\.](' + word + ')\\b(?![^<>()\"]*>)', 'gi');
+	  var rgxp = new RegExp('\\b[^\.?](' + word + ')\\b(?![^<>()\"]*>)', 'gi');
 	  var print = JSON.stringify(item);
 	  print = print.replace(/\'/g, "");
 	  if (!item.length) {
