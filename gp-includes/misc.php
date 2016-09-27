GlotPress/GlotPress

commit 7b103dd555979a22e27b35fe5cb44bb6b28411dc

Accept "0" as a translation value.
Introduce a new validation rule for empty strings and use it for the `translation_0` field of `GP_Translation`.

fixes #413.

gp-includes/misc.php
@@ -334,7 +334,7 @@ function gp_has_translation_been_updated( $translation_set, $timestamp = 0 ) {
  * @param int $id translation set ID
  */
 function gp_clean_translation_set_cache( $id ) {
-	wp_cache_delete( $id, 'translation_set_status_breakdown' ); 
+	wp_cache_delete( $id, 'translation_set_status_breakdown' );
 	wp_cache_delete( $id, 'translation_set_last_modified' );
 }
 
@@ -393,6 +393,26 @@ function gp_time_since( $time ) {
  */
 function gp_is_empty( $value ) {
 	return empty( $value );
+}
+
+/**
+ * Checks if the passed value is an empty string.
+ *
+ * @param string $value The value you want to check.
+ * @return bool
+ */
+function gp_is_empty_string( $value ) {
+	return '' === $value;
+}
+
+/**
+ * Checks if the passed value isn't an empty string.
+ *
+ * @param string $value The value you want to check.
+ * @return bool
+ */
+function gp_is_not_empty_string( $value ) {
+	return '' !== $value;
 }
 
 /**

gp-templates/translation-row.php
@@ -30,7 +30,7 @@ $can_reject_self = (GP::$user->current()->user_login == $t->user_login && $t->tr
 	<?php
 		$edit_text = $can_edit? __('Double-click to add') : sprintf(__('You <a href="%s">have to login</a> to add a translation.'), gp_url_login());
 		$missing_text = "<span class='missing'>$edit_text</span>";
-		if ( !count( array_filter( $t->translations ) ) ):
+		if ( ! count( array_filter( $t->translations, 'gp_is_not_empty_string' ) ) ):
 			echo $missing_text;
 		elseif ( !$t->plural ):
 			echo esc_translation( $t->translations[0] );
@@ -39,7 +39,7 @@ $can_reject_self = (GP::$user->current()->user_login == $t->user_login && $t->tr
 			<?php
 				foreach( $t->translations as $translation ):
 			?>
-				<li><?php echo $translation? esc_translation( $translation ) : $missing_text; ?></li>
+				<li><?php echo gp_is_empty_string( $translation ) ? $missing_text : esc_translation( $translation ); ?></li>
 			<?php
 				endforeach;
 			?>

gp-includes/things/translation.php
@@ -56,7 +56,7 @@ class GP_Translation extends GP_Thing {
 	}
 
 	function restrict_fields( $translation ) {
-		$translation->translation_0_should_not_be( 'empty' );
+		$translation->translation_0_should_not_be( 'empty_string' );
 		$translation->status_should_not_be( 'empty' );
 		$translation->original_id_should_be( 'positive_int' );
 		$translation->translation_set_id_should_be( 'positive_int' );

gp-includes/validation.php
@@ -114,6 +114,7 @@ class GP_Validators {
 }
 
 GP_Validators::register( 'empty', 'gp_is_empty' );
+GP_Validators::register( 'empty_string', 'gp_is_empty_string' );
 GP_Validators::register( 'positive_int', 'gp_is_positive_int' );
 GP_Validators::register( 'int', 'gp_is_int' );
 GP_Validators::register( 'null', 'gp_is_null' );

commit aeb3b48e5e8fd3ea85c5779e2afe9ca566c28ff2

Restore edit text for non translated entries.
Broken since 7b103dd.
Fixes #519.

gp-includes/misc.php
@@ -343,7 +343,17 @@ function gp_is_int( $value ) {
  * @return bool
  */
 function gp_is_null( $value ) {
-	return is_null( $value );
+	return null === $value;
+}
+
+/**
+ * Checks if the passed value is not null.
+ *
+ * @param string $value The value you want to check.
+ * @return bool
+ */
+function gp_is_not_null( $value ) {
+	return null !== $value;
 }
 
 /**

gp-templates/translation-row.php
@@ -36,13 +36,13 @@ $can_reject_self = ( isset( $t->user->user_login ) && $user->user_login === $t->
 			$edit_text = __( 'You are not allowed to add a translation.', 'glotpress' );
 		}
 		else {
-			$edit_text = sprintf( __( 'You <a href="%s">have to login</a> to add a translation.', 'glotpress' ), esc_url( wp_login_url() ) );
+			$edit_text = sprintf( __( 'You <a href="%s">have to log in</a> to add a translation.', 'glotpress' ), esc_url( wp_login_url( gp_url_current() ) ) );
 		}
 
 		$missing_text = "<span class='missing'>$edit_text</span>";
-		if ( ! count( array_filter( $t->translations, 'gp_is_not_empty_string' ) ) ):
+		if ( ! count( array_filter( $t->translations, 'gp_is_not_null' ) ) ) :
 			echo $missing_text;
-		elseif ( !$t->plural ):
+		elseif ( ! $t->plural ) :
 			echo esc_translation( $t->translations[0] );
 		else: ?>
 		<ul>