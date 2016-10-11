8c6227c8c25a040db78696e3bcaa7b6d0c1b0023
Greg Ross <greg@toolstack.com>
2016-10-11 12:59:24 -0400

    Bump css version on enqueue.

--- gp-includes/assets-loader.php
+++ gp-includes/assets-loader.php
@@ -15,10 +15,10 @@ function gp_register_default_styles() {
 	$suffix = SCRIPT_DEBUG ? '.css' : '.min.css';
 
 	// Register our base style.
-	wp_register_style( 'gp-base', $url . '/style' . $suffix, array(), '20150717' );
+	wp_register_style( 'gp-base', $url . '/style' . $suffix, array(), '20161011' );
 
 	// Register the table sorter theme style.
-	wp_register_style( 'tablesorter-theme', $url . '/tablesorter.theme.glotpress' . $suffix, array(), '20150717' );
+	wp_register_style( 'tablesorter-theme', $url . '/tablesorter.theme.glotpress' . $suffix, array(), '20161011' );
 }
 
 add_action( 'init', 'gp_register_default_styles' );
