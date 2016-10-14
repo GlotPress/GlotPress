5441f4f9e8ef28eaa8bf5773eb6c9525cea1db9b
Daniele Scasciafratte <mte90net@gmail.com>
2016-10-14 11:01:09 +0200

    fix loading problem of keymaster on chrome

--- init.js
+++ init.js
@@ -1,16 +1,17 @@
-//Load jQuery Hotkeys
+//Load Keymaster
 s = document.createElement('script');
 s.src = chrome.extension.getURL('keymaster.js');
 s.type = 'text/javascript';
-s.onload = function() {
-    this.parentNode.removeChild(this);
+s.onload = function () {
+  this.parentNode.removeChild(this);
+  //Load GlotDict
+  s = document.createElement('script');
+  s.src = chrome.extension.getURL('glotdict.js');
+  s.type = 'text/javascript';
+  s.onload = function () {
+	this.parentNode.removeChild(this);
+  };
+  (document.head || document.documentElement).appendChild(s);
 };
-(document.head || document.documentElement).appendChild(s); 
-//Load GlotDict
-s = document.createElement('script');
-s.src = chrome.extension.getURL('glotdict.js');
-s.type = 'text/javascript';
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(s); 
+(document.head || document.documentElement).appendChild(s);
+
