7ba3ccef9e2ed1b23560e255e007827f7f1b1b07
Daniele Scasciafratte <mte90net@gmail.com>
2017-01-05 12:31:56 +0100

    fix #66

---
+++ js/init.js
@@ -0,0 +1,29 @@
+jsScripts = ['jquery.bind-first', 'keymaster', 'pluralize', 'glotdict'];
+script(jsScripts);
+
+function script(url) {
+  if (Array.isArray(url)) {
+	var self = this, prom = [];
+	url.forEach(function (item) {
+	  prom.push(self.script(item));
+	});
+	return Promise.all(prom);
+  }
+  return new Promise(function (resolve, reject) {
+	var r = false,
+			t = document.getElementsByTagName("script")[0],
+			s = document.createElement("script");
+	s.type = "text/javascript";
+	s.src = chrome.extension.getURL('js/' + url + '.js');
+	;
+	s.async = true;
+	s.onload = s.onreadystatechange = function () {
+	  if (!r && (!this.readyState || this.readyState === "complete")) {
+		r = true;
+		resolve(this);
+	  }
+	};
+	s.onerror = s.onabort = reject;
+	t.parentNode.insertBefore(s, t);
+  });
+}
