d43af049bae308ffd29ed40a4122d22b549a57d4
Greg Ross <greg@toolstack.com>
2017-02-10 16:56:43 -0500

    Rewrite the xliff code to strip the tags out and add them to the comments.

--- gp-includes/formats/format-android.php
+++ gp-includes/formats/format-android.php
@@ -61,8 +61,8 @@ class GP_Format_Android extends GP_Format {
 		 * Unfortunately SimpleXML will parse these as valid XML tags, which we don't want so encapsulate them in a CDATA structure
 		 * that we can tell SimpleXML to ignore.
 		*/
-		$contents = str_replace( '<xliff:g', '<![CDATA[<xliff:g', $contents );
-		$contents = str_replace( '</xliff:g>', '</xliff:g>]]>', $contents );
+		$contents = str_ireplace( '<xliff:g', '--xlifftag--xliff:g', $contents );
+		$contents = str_ireplace( '</xliff:g>', '--xlifftag--/xliff:g>', $contents );
 
 		$data = simplexml_load_string( $contents, null, LIBXML_NOCDATA );
 		libxml_use_internal_errors( $errors );
@@ -77,6 +77,13 @@ class GP_Format_Android extends GP_Format {
 				continue;
 			}
 
+			$xliff_info = $this->extract_xliff_info( (string)$string[0] );
+			
+			if ( false !== $xliff_info ) {
+				$string[0] = $xliff_info['string'];
+				$string['comment'] .= $xliff_info['description'];
+			}
+			
 			$entry = new Translation_Entry();
 			$entry->context = (string)$string['name'];
 			$entry->singular = $this->unescape( (string)$string[0] );
@@ -114,7 +121,86 @@ class GP_Format_Android extends GP_Format {
 		return $entries;
 	}
 
+	function extract_xliff_info( $string ) {
+		// Define the initial xliff tag to look for.
+		$search = '--xlifftag--';
+		
+		/*
+		 * If it's not in the string, don't do any more processing.  Note we don't need to worry about
+		 * case sensitivity here as a search and replace was done earlier which forced them all to the
+		 * exact string above.
+		 */
+		if ( false === strstr( $string, $search ) ) {
+			return false;
+		}
+		
+		$string = str_ireplace( $search, '<', $string );
+		
+		// Break apart the string in case there are multiple xliff's in it.
+		$parts = explode( '<xliff:g', $string );
 
+		// Setup the results array, part 0 will never need to be processed so automatically add it to the returned string.
+		$result = array();
+		$result['string'] = $parts[0];
+		$result['comment'] = '';
+		$result['description'] = '';
+
+		// As we can skip the first part, loop through only the remaining parts.
+		for ( $i = 1; $i < sizeof( $parts ); $i++ ) {
+			// Add back the part we stripped out during the explode() above.
+			$current =  '<xliff:g' . $parts[$i];
+		
+			$matches = array();
+
+			/*
+			 * Break apart the entire string in to 5 parts:
+			 * 
+			 *     0 = The full string.
+			 *     1 = Any text before the xliff tag.
+			 *     2 = The opening xliff tag.
+			 *     3 = The actual text to be translated.
+			 *     4 = The closing xliff tag.
+			 *     5 = The rest of the string.
+			 */
+			if ( false !== preg_match( '/(.*)(<xliff:g.*>)(.*)(<\/xliff:g>)(.*)/i', $current, $matches ) ) {
+				// If we have a match add to the two parameters to return the correct parts of the match.
+				$result['string'] .= $matches[1] . $matches[3] . $matches[5];
+				$result['comment'] .= ' ' . $matches[2] . $matches[3] . $matches[4];
+				
+				$component = $matches[3];
+				$text = '';
+				
+				$id = preg_match( '/.*id="(.*)".*/iU', $result['comment'], $matches );
+				if ( false !== $id ) {
+					$id = $matches[1];
+				}
+				
+				$example = preg_match( '/.*example="(.*)".*/iU', $result['comment'], $matches );
+				if ( false !== $example ) {
+					$example = $matches[1];
+				}
+						
+				if ( false !== $id && false !== $example ) {
+					$text = sprintf( __( 'This string has content that should not be translated, the "%s" component of the original, which is identified as the "%s" attribute by the developer may be replaced at run time with text like this: %s', 'glotpress' ), $component, $id, $example );
+				} else if ( false !== $id ) {
+					$text = sprintf( __( 'This string has content that should not be translated, the "%s" component of the original, which is identified as "%s" by the developer and is not intendent to be translated.', 'glotpress' ), $component, $id );
+				} else if ( false !== $example ) {
+					$text = sprintf( __( 'This string has content that should not be translated, the "%s" component of the original may be replaced at run time with text like this: %s', 'glotpress' ), $component, $example );
+				}
+						
+				$result['description'] .= ' ' . $text;
+			} else {
+				// If we don't, just append the current string to the result.
+				$result['string'] .= ' ' . $current;
+			}
+		}	
+
+		$result['comment'] = trim( $result['comment'] );
+		$result['description'] = trim( $result['description'] );
+		
+		return $result;
+	}
+	
 	private function line( $string, $prepend_tabs = 0 ) {
 		$this->exported .= str_repeat( "\t", $prepend_tabs ) . "$string\n";
 	}
