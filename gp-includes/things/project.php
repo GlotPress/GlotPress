d0314ca8d53b41d91ef0de0ec7dd5aa066f2a8b7
Dominik Schilling <dominikschilling+git@gmail.com>
2017-02-21 22:30:47 +0100

    Add missing docs for filters and rename variable

--- gp-includes/things/project.php
+++ gp-includes/things/project.php
@@ -44,9 +44,16 @@ class GP_Project extends GP_Thing {
 	// Additional queries
 
 	public function by_path( $path ) {
-		$locale_glossary_prefix = apply_filters( 'gp_locale_glossary_path_prefix', '/languages' );
+		/**
+		 * Filters the prefix for the locale glossary path.
+		 *
+		 * @since 2.3.1
+		 *
+		 * @param string $$locale_glossary_path_prefix Prefix for the locale glossary path.
+		 */
+		$locale_glossary_path_prefix = apply_filters( 'gp_locale_glossary_path_prefix', '/languages' );
 
-		if ( $locale_glossary_prefix === $path ) {
+		if ( $locale_glossary_path_prefix === $path ) {
 			return GP::$glossary->get_locale_glossary_project();
 		}
 		return $this->one( "SELECT * FROM $this->table WHERE path = %s", trim( $path, '/' ) );
