87cff59bc181c57fdfa0b1d41ad454660c461ab7
Daniele Scasciafratte <mte90net@gmail.com>
2017-02-28 23:07:22 +0100

    updated tests

--- js/glotdict.js
+++ js/glotdict.js
@@ -437,8 +437,9 @@ jQuery(document).ready(function () {
 	}
 	jQuery("<style type='text/css'>.has-glotdict td:first-child,.has-glotdict th:first-child,.box.has-glotdict{border-left-width: 2px !important;border-left-color: blue !important;}</style>").appendTo("head");
 	jQuery("<div class='box has-glotdict'></div><div>Contain a GlotDict term</div>").appendTo("#legend");
-	gd_terms_tooltip();
+	
 	gd_locales_selector();
+	gd_terms_tooltip();
 
 	jQuery($gp.editor.table).onFirst('click', 'button.ok', gd_validate);
   }
