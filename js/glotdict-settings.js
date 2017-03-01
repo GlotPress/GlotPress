b4967a00b3caa8b46e3d78a53225ce4dad712d45
Daniele Scasciafratte <mte90net@gmail.com>
2017-03-01 16:40:45 +0100

    settings panel done

--- js/glotdict-settings.js
+++ js/glotdict-settings.js
@@ -1,3 +1,45 @@
 /* global key, glotdict_version, $gp, pluralize */
 
 jQuery('#menu-headline-nav').append('<li class="current-menu-item gd_setting" style="cursor:pointer;"><a>GlotDict</a></li>');
+
+jQuery('.gd_setting').click(function () {
+  gd_generate_settings_panel();
+});
+
+jQuery('.gp-content').on('click', '.gd_settings_panel .gd_setting_check', function () {
+  localStorage.setItem(jQuery(this).attr('id'), jQuery(this).is(':checked'));
+});
+
+function gd_generate_settings_panel() {
+  if (jQuery('.gd_settings_panel').length !== 0) {
+	jQuery('.gd_settings_panel').toggle();
+	return;
+  }
+  var settings = {
+	'no_final_dot': 'No validation for final "...", ".", ":"',
+	'no_final_other_dots': 'No validation for final ;.!:、。؟？！'
+  };
+  var container = '<div class="notice gd_settings_panel"><h2>GlotDict Settings</h2></div>';
+  jQuery('.gp-content').prepend(container);
+  var hotkeys = '<h3>Hotkeys</h3><ul>' +
+		  '<li>Shortcut on Ctrl+Shift+Z to click "Cancel"</li>' +
+		  '<li>Shortcut on Ctrl+Shift+A to click "Approve"</li>' +
+		  '<li>Shortcut on Ctrl+Shift+R to click "Reject"</li>' +
+		  '<li>Shortcut on Ctrl+Shift+F to click "Fuzzy"</li>' +
+		  '<li>Shortcut on Ctrl+Enter to click "Suggest new translation" or "Add translation"</li>' +
+		  '<li>Shortcut on Page Down to open the previous string to translate</li>' +
+		  '<li>Shortcut on Page Up to open the next string to translate</li>' +
+		  '<li>Shortcut on Ctrl+Shift+B to "Copy from original"</li>' +
+		  '<li>Shortcut on Ctrl+Shift+F to add non-breaking spaces near symbols</li>' +
+		  '<li>Shortcut on Ctrl+Alt+R to reset all the GlotDict settings</li>' +
+		  '<li>Right click of the mouse on the term with a dashed line and the translation will be added in the translation area</li>' +
+		  '</ul><br><h3>Settings</h3>';
+  jQuery('.gd_settings_panel').append(hotkeys);
+  jQuery.each(settings, function (key, value) {
+	var checked = '';
+	if (localStorage.getItem('gd_' + key) === 'true') {
+	  checked = 'checked';
+	}
+	jQuery('.gd_settings_panel').append('<input class="gd_setting_check" type="checkbox" id="gd_' + key + '" ' + checked + '> ' + value + '<br>');
+  });
+}
