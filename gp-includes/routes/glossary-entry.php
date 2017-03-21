5dd5c0e0b87d9dc3530b7e3c280f5cb1ee270cca
Alin Marcu <admin@deconf.com>
2017-03-21 19:20:55 +0200

    Allow users that can import glossaries to flush them at import

--- gp-includes/routes/glossary-entry.php
+++ gp-includes/routes/glossary-entry.php
@@ -236,7 +236,7 @@ class GP_Route_Glossary_Entry extends GP_Route_Main {
 			return;
 		}
 
-		$can_write = $this->can( 'write', 'project', $project->id );
+		$can_edit = $this->can( 'approve', 'translation-set', $translation_set->id );
 
 		$this->tmpl( 'glossary-import', get_defined_vars() );
 	}
@@ -275,7 +275,7 @@ class GP_Route_Glossary_Entry extends GP_Route_Main {
 		}
 
 		$replace = gp_post( 'import-flush' );
-		if ( 'on' === $replace && $this->can( 'write', 'project', $project->id ) ) {
+		if ( 'on' === $replace && $this->can( 'approve', 'translation-set', $translation_set->id ) ) {
 			GP::$glossary_entry->delete_many( array( 'glossary_id' => $glossary->id ) );
 		}
 
