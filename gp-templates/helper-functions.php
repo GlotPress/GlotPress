e25eb473d7dc961147cfa57de16bc95bac0012cd
Greg Ross <greg@toolstack.com>
2017-08-22 18:06:45 -0400

    Use selected() to simplfy code.

--- gp-templates/helper-functions.php
+++ gp-templates/helper-functions.php
@@ -304,12 +304,7 @@ function gp_translations_bulk_actions_toolbar( $bulk_action, $can_write, $locati
 	];
 
 	foreach ( GP::$original->get_static( 'priorities' ) as $value => $label ) {
-		if ( isset( $labels[ $label ] ) ) {
-			$label = $labels[ $label ];
-		}
-
-		$selected = 'normal' === $value ? " selected='selected'" : '';
-		echo "\t<option value='" . esc_attr( $value ) . "' $selected>" . esc_html( $label ) . "</option>\n"; // WPCS: XSS Ok.
+		echo "\t<option value='" . esc_attr( $value ) . "' " . selected( 'normal', $value, false )  . ">" . esc_html( $label ) . "</option>\n"; // WPCS: XSS Ok.
 	}
 	?>
 	</select>
