3d70c83e038f49325814206e6b70ecda68fd0632
pedro-mendonca <ped.gaspar@gmail.com>
2017-09-27 00:14:22 +0100

    Add `Action` context to bulk actions to comply with PR #813

--- gp-templates/helper-functions.php
+++ gp-templates/helper-functions.php
@@ -262,9 +262,9 @@ function gp_translations_bulk_actions_toolbar( $bulk_action, $can_write, $locati
 	<div>
 	<select name="bulk[action]" id="bulk-action-<?php echo esc_attr( $location ); ?>" class="bulk-action">
 		<option value="" selected="selected"><?php _e( 'Bulk Actions', 'glotpress' ); ?></option>
-		<option value="approve"><?php _e( 'Approve', 'glotpress' ); ?></option>
-		<option value="reject"><?php _e( 'Reject', 'glotpress' ); ?></option>
-		<option value="fuzzy"><?php _e( 'Fuzzy', 'glotpress' ); ?></option>
+		<option value="approve"><?php _ex( 'Approve', 'Action', 'glotpress' ); ?></option>
+		<option value="reject"><?php _ex( 'Reject', 'Action', 'glotpress' ); ?></option>
+		<option value="fuzzy"><?php _ex( 'Fuzzy', 'Action', 'glotpress' ); ?></option>
 	<?php if ( $can_write ) : ?>
 		<option value="set-priority" class="hide-if-no-js"><?php _e( 'Set Priority', 'glotpress' ); ?></option>
 	<?php endif; ?>
