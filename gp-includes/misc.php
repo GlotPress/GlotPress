85559b470dcec26afde46b67dd1429239113f43a
Greg Ross <greg@toolstack.com>
2018-10-04 15:17:15 -0400

    Revert "Refactor gp_array_zip to remove each function."
    This reverts commit fb1f49425de92db3e873ef2db43dff9bfe1a74c8.

--- gp-includes/misc.php
+++ gp-includes/misc.php
@@ -163,40 +163,35 @@ function gp_populate_notices() {
  */
 function gp_array_zip() {
 	$args = func_get_args();
-
 	if ( !is_array( $args ) ) {
 		return false;
 	}
-
 	if ( empty( $args ) ) {
 		return array();
 	}
-
-	$depth = 0;
 	$res = array();
 	foreach ( $args as &$array ) {
 		if ( !is_array( $array) ) {
 			return false;
 		}
-
 		reset( $array );
-		$array_size = sizeof( $array );
-
-		if ( 0 === $depth || $depth > $array_size ) {
-			$depth = $array_size;
-		}
 	}
-
-	for ( $i = 0; $i < $depth; $i++) {
-		$array_count = 0;
-
+	$all_have_more = true;
+	while (true) {
+		$this_round = array();
 		foreach ( $args as &$array ) {
-			$res[ $i ][ $array_count ] = $array[ $i ];
-
-			$array_count++;
+			$all_have_more = ( list( , $value ) = each( $array ) );
+			if ( !$all_have_more ) {
+				break;
+			}
+			$this_round[] = $value;
+		}
+		if ( $all_have_more ) {
+			$res[] = $this_round;
+		} else {
+			break;
 		}
 	}
-
 	return $res;
 }
 
