319154a7e5cda20b5f27244d0b924d2f14226887
Garrett Hyder <garrett@eclipsecreative.ca>
2018-10-12 12:58:08 -0700

    Removed part_of_speech from the  so as to avoid blocking multiple noun translations. Fixes #946 and Reverts some of the unintentional code merged to resolve #745

--- gp-includes/routes/glossary-entry.php
+++ gp-includes/routes/glossary-entry.php
@@ -90,7 +90,6 @@ class GP_Route_Glossary_Entry extends GP_Route_Main {
 				$find_parms = array(
 					'glossary_id'    => $glossary->id,
 					'term'           => $new_glossary_entry->term,
-					'part_of_speech' => $new_glossary_entry->part_of_speech,
 				);
 
 				if ( GP::$glossary_entry->find_one( $find_parms ) ) {
