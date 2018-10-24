c8827db1aaf4d4468ea44c54234f615b4c1e8d03
Daniele Scasciafratte <mte90net@gmail.com>
2018-10-24 17:47:02 +0200

    adding test calss for note route

--- gp-includes/routes/note.php
+++ gp-includes/routes/note.php
@@ -34,8 +34,9 @@ class GP_Route_Note extends GP_Route_Main {
 		$noteObject = GP::$notes->save( $note, $translation );
 
 		$this->render_note($noteObject, $translation);
+		return true;
 	}
-	
+
 	/**
 	 * Processes the note set action to set on the translation.
 	 *
@@ -47,7 +48,7 @@ class GP_Route_Note extends GP_Route_Main {
 		$translation_id = gp_post( 'translation_id' );
 		$note = gp_post( 'note' );
 		$note_id = gp_post( 'note_id' );
- 
+
 		if ( ! $this->verify_nonce( 'edit-note-' . $note_id ) ) {
 			return $this->die_with_error( __( 'An error has occurred. Please try again.', 'glotpress' ), 403 );
 		}
@@ -56,8 +57,9 @@ class GP_Route_Note extends GP_Route_Main {
 		$noteObject = GP::$notes->edit( $note_id, $note, $translation );
 
 		$this->render_note( $noteObject, $translation );
+		return true;
 	}
-	
+
 	/**
 	 * Processes the note set action to set on the translation.
 	 *
@@ -66,24 +68,23 @@ class GP_Route_Note extends GP_Route_Main {
 	 * @return bool
 	 */
 	public function delete_post() {
-		
 		$note_id = gp_post( 'note_id' );
 
 		if ( ! $this->verify_nonce( 'delete-note-' . $note_id ) ) {
 			return $this->die_with_error( __( 'An error has occurred. Please try again.', 'glotpress' ), 403 );
 		}
+
 		GP::$notes->delete_all( array('id' => $note_id) );
 
 		return true;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param object $note
 	 * @param object $translation
 	 */
-	private function render_note( $note, $translation )
-	{
+	private function render_note( $note, $translation )	{
 		require_once GP_TMPL_PATH . 'helper-functions.php';
 		$can_approve = $this->can( 'approve', 'translation-set', $translation->translation_set_id );
 		render_note($note, $can_approve);
