63bca3652e67d9b496657cadf626a5d716ccabbe
Daniele Scasciafratte <mte90net@gmail.com>
2018-10-24 17:47:02 +0200

    adding test calss for note route

---
+++ tests/phpunit/testcases/tests_routes/test_route_note.php
@@ -0,0 +1,20 @@
+<?php
+
+class GP_Test_Route_Note extends GP_UnitTestCase_Route {
+	public $route_class = 'GP_Route_Note';
+
+	function test_add_note_function() {
+		$this->set_admin_user_as_current();
+
+		$set = $this->factory->translation_set->create_with_project_and_locale();
+		$original = $this->factory->original->create( array( 'project_id' => $set->project->id, 'status' => '+active', 'singular' => 'baba' ) );
+
+		$translation = $this->factory->translation->create( array(
+			'translation_set_id' => $set->id,
+			'original_id'        => $original->id,
+			'status'             => 'current',
+		) );
+		$translation->set_as_current();
+
+	}
+}
