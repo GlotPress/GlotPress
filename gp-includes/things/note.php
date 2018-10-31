6a65261e58d6bf01ed5baa8808d56817a50193d1
Daniele Scasciafratte <mte90net@gmail.com>
2018-10-31 19:50:22 +0100

    fix typos and others stuff

--- gp-includes/things/note.php
+++ gp-includes/things/note.php
@@ -72,10 +72,9 @@ class GP_Note extends GP_Thing {
 	 * @return object The output of the query.
 	 */
 	public function save( $args = null ) {
-		global $wpdb;
 		$original_id    = gp_post( 'original_id' );
 		$translation_id = gp_post( 'translation_id' );
-		$note           = trim( gp_post( 'note' ) );
+		$note           = trim( esc_html ( gp_post( 'note' ) ) );
 		$translation    = new GP_Translation( array( 'id' => $translation_id ) );
 		if ( ! GP::$permission->current_user_can( 'approve', 'translation', $translation_id, array( 'translation' => $translation ) ) ) {
 			return false;
