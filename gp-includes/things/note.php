9a031ec57b346ffb915a7e34d9b0c336dafca296
Daniele Scasciafratte <mte90net@gmail.com>
2018-11-02 22:43:55 +0100

    moving to templates

--- gp-includes/things/note.php
+++ gp-includes/things/note.php
@@ -108,21 +108,21 @@ class GP_Note extends GP_Thing {
 	}
 
 	/**
-	 * Retrieves the note for this entry.
+	 * Retrieves the note for this translation.
 	 *
 	 * @since 3.0.0
 	 *
-	 * @param object $entry The translation entry.
+	 * @param object $translation The translation entry.
 	 * @param object $order Order but not used.
 	 *
 	 * @return array notes
 	 */
-	public function get_by_entry( $entry, $order = null ) {
+	public function get_by_translation( $translation, $order = null ) {
 		return $this->many(
 			$this->select_all_from_conditions_and_order(
 				array(
-					'original_id'        => $entry->original_id,
-					'translation_set_id' => $entry->translation_set_id,
+					'original_id'        => $translation->original_id,
+					'translation_set_id' => $translation->translation_set_id,
 				),
 				$order
 			)
