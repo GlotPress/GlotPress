e1c3115cd4c445430fda304ec28af7574a8dc2ad
Daniele Scasciafratte <mte90net@gmail.com>
2018-11-12 14:45:58 +0100

    show notes only if there are

--- gp-templates/notes.php
+++ gp-templates/notes.php
@@ -8,6 +8,7 @@
 
 $notes = GP::$notes->get_by_translation_id( $translation->id );
 if ( $translation->translation_status ) {
+	if ( count( $notes ) > 0 ) {
 ?>
 	<dl>
 		<dt>
@@ -23,21 +24,22 @@ if ( $translation->translation_status ) {
 	</dl>
 	<dl>
 	<?php
+	}
 
-		if ( GP::$permission->current_user_can(
-			'approve',
-			'translation',
-			$translation->id,
-			array(
-				'translation' => $translation,
-			)
-		) || get_current_user_id() === $translation->user_id ) {
+	if ( GP::$permission->current_user_can(
+		'approve',
+		'translation',
+		$translation->id,
+		array(
+			'translation' => $translation,
+		)
+	) || get_current_user_id() === $translation->user_id ) {
 		echo '<dt><br>' . __( 'New Reviewer note:', 'glotpress' ) . '</dt><br>';
 	?>
 			<dt><textarea autocomplete="off" class="foreign-text" name="note[<?php echo esc_attr( $translation->row_id ); ?>]" id="note_<?php echo esc_attr( $translation->row_id ); ?>"></textarea></dt>
 			<dt><button class="add-note" tabindex="-1" data-nonce="<?php echo esc_attr( wp_create_nonce( 'new-note-' . $translation->id ) ); ?>"><?php _e( 'Add note', 'glotpress' ); ?></button></dt>
 	<?php
-		}
+	}
 	?>
 	</dl>
 <?php
