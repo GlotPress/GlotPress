1c620342c01e5c3a5b4d2ef521f72b8a1d46bccb
Dominik Schilling <dominikschilling+git@gmail.com>
2020-04-13 11:39:28 +0200

    Remove default urldecoding from gp_urldecode_deep(). (#1054)
    * Remove now unused gp_urldecode_deep().
    * Improve docs for gp_post() and gp_get().

--- gp-includes/system.php
+++ gp-includes/system.php
@@ -1,10 +1,5 @@
 <?php
 
-function gp_urldecode_deep($value) {
-	$value = is_array( $value ) ? array_map( 'gp_urldecode_deep', $value ) : urldecode( $value );
-	return $value;
-}
-
 /**
  * Makes all key/value pairs in $vars global variables
  */
