GlotPress/GlotPress

commit 8decc5d624f48db427b41fc640828df13c829434

Fix WPCS: WordPress.Arrays... (#1066)
* Fix WPCS: WordPress.Arrays.CommaAfterArrayItem.CommaAfterLast

* Fix WPCS: WordPress.Arrays.CommaAfterArrayItem.NoComma

* Fix WPCS: WordPress.Arrays.ArrayDeclarationSpacing.SpaceInEmptyArray

* Fix WPCS: WordPress.Arrays.ArrayDeclarationSpacing.NoSpaceAfterArrayOpener

* Fix WPCS: WordPress.Arrays.ArrayDeclarationSpacing.NoSpaceBeforeArrayCloser

* Fix WPCS: WordPress.Arrays.ArrayDeclarationSpacing.ArrayItemNoNewLine

* Fix WPCS: WordPress.Arrays.ArrayKeySpacingRestrictions.NoSpacesAroundArrayKeys

* Fix WPCS: WordPress.Arrays.ArrayKeySpacingRestrictions.SpacesAroundArrayKeys

* Fix WPCS: WordPress.Arrays.MultipleStatementAlignment.DoubleArrowNotAligned

* Fix WPCS: WordPress.Arrays.ArrayDeclarationSpacing.AssociativeArrayFound

* Fix WPCS: WordPress.Arrays.ArrayDeclarationSpacing.CloseBraceNewLine

* Fix WPCS: WordPress.Arrays.MultipleStatementAlignment.DoubleArrowNotAligned

* Fix WPCS: WordPress.Arrays.CommaAfterArrayItem.NoComma

Co-authored-by: Dominik Schilling <dominikschilling+git@gmail.com>

gp-includes/things/translation.php
@@ -227,7 +227,7 @@ class GP_Translation extends GP_Thing {
 			// Reduce range by one since we're starting at 0, see GH#516.
 			foreach ( range( 0, $this->get_static( 'number_of_plural_translations' ) - 1 ) as $i ) {
 				if ( isset( $args['translations'][ $i ] ) ) {
-					$args["translation_$i"] = $args['translations'][ $i ];
+					$args[ "translation_$i" ] = $args['translations'][ $i ];
 				}
 			}
 			unset( $args['translations'] );
@@ -332,12 +332,15 @@ class GP_Translation extends GP_Thing {
 		if ( ! is_array( $default_sort ) ) {
 			$default_sort = array(
 				'by'  => 'priority',
-				'how' => 'desc'
+				'how' => 'desc',
 			);
 		}
 
 		$sort_by = gp_array_get( $sort_bys, gp_array_get( $sort, 'by' ),  gp_array_get( $sort_bys, $default_sort['by'] ) );
-		$sort_hows = array('asc' => 'ASC', 'desc' => 'DESC', );
+		$sort_hows = array(
+			'asc'  => 'ASC',
+			'desc' => 'DESC',
+		);
 		$sort_how = gp_array_get( $sort_hows, gp_array_get( $sort, 'how' ), gp_array_get( $sort_hows, $default_sort['how'] ) );
 		$collation = 'yes' === gp_array_get( $filters, 'case_sensitive' ) ? 'BINARY' : '';
 
@@ -640,12 +643,27 @@ class GP_Translation extends GP_Thing {
 
 	public function set_as_current() {
 		$result = $this->update( array( 'status' => 'old' ),
-		array( 'original_id' => $this->original_id, 'translation_set_id' => $this->translation_set_id, 'status' => 'current' ) )
+		array(
+			'original_id'        => $this->original_id,
+			'translation_set_id' => $this->translation_set_id,
+			'status'             => 'current',
+		) )
 		&& $this->update( array( 'status' => 'old' ),
-		array( 'original_id' => $this->original_id, 'translation_set_id' => $this->translation_set_id, 'status' => 'waiting' ) )
+		array(
+			'original_id'        => $this->original_id,
+			'translation_set_id' => $this->translation_set_id,
+			'status'             => 'waiting',
+		) )
 		&& $this->update( array( 'status' => 'old' ),
-		array( 'original_id' => $this->original_id, 'translation_set_id' => $this->translation_set_id, 'status' => 'fuzzy' ) )
-		&& $this->save( array( 'status' => 'current', 'user_id_last_modified' => get_current_user_id() ) );
+		array(
+			'original_id'        => $this->original_id,
+			'translation_set_id' => $this->translation_set_id,
+			'status'             => 'fuzzy'M,
+		) )
+		&& $this->save( array(
+			'status'                => 'current',
+			'user_id_last_modified' => get_current_user_id(),
+		) );
 
 		return $result;
 	}
@@ -721,7 +739,10 @@ class GP_Translation extends GP_Thing {
 		if ( 'current' === $status ) {
 			$updated = $this->set_as_current();
 		} else {
-			$updated = $this->save( array( 'user_id_last_modified' => get_current_user_id(), 'status' => $status ) );
+			$updated = $this->save( array(
+				'user_id_last_modified' => get_current_user_id(),
+				'status'                => $status,
+			) );
 		}
 
 		if ( $updated ) {

gp-includes/routes/translation.php
@@ -293,7 +293,10 @@ class GP_Route_Translation extends GP_Route_Main {
 			$data['warnings'] = GP::$translation_warnings->check( $original->singular, $original->plural, $translations, $locale );
 
 
-			$existing_translations = GP::$translation->for_translation( $project, $translation_set, 'no-limit', array('original_id' => $original_id, 'status' => 'current_or_waiting' ), array() );
+			$existing_translations = GP::$translation->for_translation( $project, $translation_set, 'no-limit', array(
+				'original_id' => $original_id,
+				'status'      => 'current_or_waiting',
+			), array() );
 			foreach ( $existing_translations as $e ) {
 				if ( array_pad( $translations, $locale->nplurals, null ) == $e->translations ) {
 					return $this->die_with_error( __( 'Identical current or waiting translation already exists.', 'glotpress' ), 200 );
@@ -321,7 +324,7 @@ class GP_Route_Translation extends GP_Route_Main {
 					$translation->set_status( 'current' );
 				}
 
-				$translations = GP::$translation->for_translation( $project, $translation_set, 'no-limit', array('translation_id' => $translation->id), array() );
+				$translations = GP::$translation->for_translation( $project, $translation_set, 'no-limit', array( 'translation_id' => $translation->id ), array() );
 
 				if ( ! empty( $translations ) ) {
 					$translation = $translations[0];
@@ -638,7 +641,10 @@ class GP_Route_Translation extends GP_Route_Main {
 
 		call_user_func( $edit_function, $project, $locale, $translation_set, $translation );
 
-		$translations = GP::$translation->for_translation( $project, $translation_set, 'no-limit', array('translation_id' => $translation->id, 'status' => 'either'), array() );
+		$translations = GP::$translation->for_translation( $project, $translation_set, 'no-limit', array(
+			'translation_id' => $translation->id,
+			'status'         => 'either',
+		), array() );
 		if ( ! empty( $translations ) ) {
 			$translation = $translations[0];
 
@@ -669,11 +675,11 @@ class GP_Route_Translation extends GP_Route_Main {
 		}
 
 		$warning = array(
-			'project_id' => $project->id,
+			'project_id'      => $project->id,
 			'translation_set' => $translation_set->id,
-			'translation' => $translation->id,
-			'warning' => gp_post( 'key' ),
-			'user' => get_current_user_id()
+			'translation'     => $translation->id,
+			'warning'         => gp_post( 'key' ),
+			'user'            => get_current_user_id(),
 		);
 
 		/**
@@ -691,9 +697,9 @@ class GP_Route_Translation extends GP_Route_Main {
 		 */
 		do_action_ref_array( 'gp_warning_discarded', $warning );
 
-		unset( $translation->warnings[gp_post( 'index' )][gp_post( 'key' )] );
-		if ( empty( $translation->warnings[gp_post( 'index' )] ) ) {
-			unset( $translation->warnings[gp_post( 'index' )] );
+		unset( $translation->warnings[ gp_post( 'index' ) ][ gp_post( 'key' ) ] );
+		if ( empty( $translation->warnings[ gp_post( 'index' ) ] ) ) {
+			unset( $translation->warnings[ gp_post( 'index' ) ] );
 		}
 
 		$res = $translation->save();

glotpress.php
@@ -161,7 +161,12 @@ require_once GP_PATH . 'gp-settings.php';
 function gp_activate_plugin() {
 	$admins = GP::$permission->find_one( array( 'action' => 'admin' ) );
 	if ( ! $admins ) {
-		GP::$permission->create( array( 'user_id' => get_current_user_id(), 'action' => 'admin' ) );
+		GP::$permission->create(
+			array(
+				'user_id' => get_current_user_id(),
+				'action'  => 'admin',
+			)
+		);
 	}
 }
 register_activation_hook( GP_PLUGIN_FILE, 'gp_activate_plugin' );

gp-includes/cli/add-admin.php
@@ -16,7 +16,10 @@ class GP_CLI_Add_Admin extends WP_CLI_Command {
 				/* translators: %s: Username */
 				WP_CLI::error( sprintf( __( "User '%s' doesn't exist.", 'glotpress' ), $user_login ) );
 			}
-			if ( ! GP::$permission->create( array( 'user_id' => $user_to_make_admin->ID, 'action' => 'admin' ) ) ) {
+			if ( ! GP::$permission->create( array(
+				'user_id' => $user_to_make_admin->ID,
+				'action'  => 'admin',
+			) ) ) {
 				/* translators: %s: Username */
 				WP_CLI::error( sprintf( __( "Error in making '%s' an admin.", 'glotpress' ), $user_login ) );
 			}

gp-includes/cli/remove-multiple-currents.php
@@ -6,7 +6,10 @@ class GP_CLI_Remove_Multiple_Currents extends WP_CLI_Command {
 		foreach ( $sets as $set ) {
 			/* translators: %d: Set ID */
 			WP_CLI::log( sprintf( __( 'Processing set #%d..', 'glotpress' ), $set->id ) );
-			$translations = GP::$translation->find( array( 'translation_set_id' => $set->id, 'status' => 'current' ), 'original_id ASC' );
+			$translations = GP::$translation->find( array(
+				'translation_set_id' => $set->id,
+				'status'             => 'current',
+			), 'original_id ASC' );
 			$prev_original_id = null;
 			foreach ( $translations as $translation ) {
 				if ( $translation->original_id == $prev_original_id ) {

gp-includes/cli/upgrade-set-permissions.php
@@ -3,15 +3,18 @@
 class GP_CLI_Upgrade_Set_Permissions extends WP_CLI_Command {
 
 	public function __invoke() {
-		$permissions = GP::$permission->find_many( array( 'object_type' => 'translation-set', 'action' => 'approve' ) );
+		$permissions = GP::$permission->find_many( array(
+			'object_type' => 'translation-set',
+			'action'      => 'approve',
+		) );
 		foreach ( $permissions as $permission ) {
 			$set = GP::$translation_set->get( $permission->object_id );
 			$project = GP::$project->get( $set->project_id );
 			GP::$permission->create( array(
-				'user_id' => $permission->user_id,
-				'action' => 'approve',
+				'user_id'     => $permission->user_id,
+				'action'      => 'approve',
 				'object_type' => 'project|locale|set-slug',
-				'object_id' => $project->id.'|'.$set->locale.'|'.$set->slug,
+				'object_id'   => $project->id.'|'.$set->locale.'|'.$set->slug,
 			) );
 		}
 	}

gp-includes/formats/format-ngx.php
@@ -57,7 +57,7 @@ class GP_Format_NGX extends GP_Format {
 				$entrykey = substr( $key, 0, $arraykeyindex );
 				$keypair = substr( $key, $arraykeyindex + 1, strlen( $key ) - $arraykeyindex - 2 );
 				$valuepair = array(
-					'key' => $keypair,
+					'key'         => $keypair,
 					'translation' => $entry->translations[0],
 				);
 				if ( is_array( $result[ $entrykey ] ) ) {
@@ -111,7 +111,7 @@ class GP_Format_NGX extends GP_Format {
 					  && isset( $valueelem['translation'] ) ) {
 						$args = array(
 							'singular' => $valueelem['translation'],
-							'context' => $key . '[' . $valueelem['key'] . ']',
+							'context'  => $key . '[' . $valueelem['key'] . ']',
 						);
 						$entries->add_entry( new Translation_Entry( $args ) );
 					}
@@ -119,7 +119,7 @@ class GP_Format_NGX extends GP_Format {
 			} else {
 				$args = array(
 					'singular' => $value,
-					'context' => $key,
+					'context'  => $key,
 				);
 				$entries->add_entry( new Translation_Entry( $args ) );
 			}

gp-includes/meta.php
@@ -114,7 +114,12 @@ function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 
 	// If no rows are returned we need to insert the meta data instead of updating it.
 	if ( null === $cur ) {
-		$result = $wpdb->insert( $wpdb->gp_meta, array( 'object_type' => $object_type, 'object_id' => $object_id, 'meta_key' => $meta_key, 'meta_value' => $_meta_value ) );
+		$result = $wpdb->insert( $wpdb->gp_meta, array(
+			'object_type' => $object_type,
+			'object_id'   => $object_id,
+			'meta_key'    => $meta_key,
+			'meta_value'  => $_meta_value,
+		) );
 
 		// If the insert failed, return false, otherwise return the id of the inserted row.
 		if ( false === $result ) {
@@ -123,7 +128,11 @@ function gp_update_meta( $object_id = 0, $meta_key, $meta_value, $type, $global 
 			$ret = $wpdb->insert_id;
 		}
 	} else if ( $cur->meta_value !== $meta_value ) {
-		$result = $wpdb->update( $wpdb->gp_meta, array( 'meta_value' => $_meta_value ), array( 'object_type' => $object_type, 'object_id' => $object_id, 'meta_key' => $meta_key ) );
+		$result = $wpdb->update( $wpdb->gp_meta, array( 'meta_value' => $_meta_value ), array(
+			'object_type' => $object_type,
+			'object_id'   => $object_id,
+			'meta_key'    => $meta_key,
+		) );
 
 		// If the update failed, return false.
 		if ( false === $result ) {

commit ede0fd93751c628d6f5d58c50a7cdbda37bb5392

Fix typo introduced in #1066

gp-includes/things/translation.php
@@ -658,7 +658,7 @@ class GP_Translation extends GP_Thing {
 		array(
 			'original_id'        => $this->original_id,
 			'translation_set_id' => $this->translation_set_id,
-			'status'             => 'fuzzy'M,
+			'status'             => 'fuzzy',
 		) )
 		&& $this->save( array(
 			'status'                => 'current',