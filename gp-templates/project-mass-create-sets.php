d3f5fa84627531f7e5771498158cdc9a720e24d4
Pedro Mendonça <ped.gaspar@gmail.com>
2020-04-14 12:53:09 +0100

    I18N: Remove line breaks in sentence (#1071)

--- gp-templates/project-mass-create-sets.php
+++ gp-templates/project-mass-create-sets.php
@@ -15,12 +15,13 @@ wp_localize_script( 'gp-mass-create-sets-page', '$gp_mass_create_sets_options', 
 gp_tmpl_header();
 ?>
 <h2><?php _e( 'Mass-create Translation Sets', 'glotpress' ); ?></h2>
-<p><?php _e( 'Here you can mass-create translation sets in this project.
-The list of translation sets will be mirrored with the sets of a project you choose.
-Usually this is one of the parent projects.', 'glotpress' ); ?></p>
+<p>
+	<?php _e( 'Here you can mass-create translation sets in this project.', 'glotpress' ); ?><br>
+	<?php _e( 'The list of translation sets will be mirrored with the sets of a project you choose. Usually this is one of the parent projects.', 'glotpress' ); ?>
+</p>
 <form action="<?php echo esc_url( gp_url_current() ); ?>" method="post">
 	<dl>
-		<dt><label for="project_id"><?php _e( 'Project to take translation sets from:', 'glotpress' );  ?></label></dt>
+		<dt><label for="project_id"><?php _e( 'Project to take translation sets from:', 'glotpress' ); ?></label></dt>
 		<dd><?php echo gp_projects_dropdown( 'project_id', null ); ?></dd>
 	</dl>
 	<div id="preview"></div>
