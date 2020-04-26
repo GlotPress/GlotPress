81f2357c29ba8601054a5d2272c0c4388da44375
Dominik Schilling <dominikschilling+git@gmail.com>
2020-04-26 12:24:50 +0200

    Locales: Update formatting of plural expression for Ukrainian.

--- locales/locales.php
+++ locales/locales.php
@@ -2411,7 +2411,7 @@ class GP_Locales {
 		$uk->wp_locale = 'uk';
 		$uk->slug = 'uk';
 		$uk->nplurals = 3;
-		$uk->plural_expression = '(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)';
+		$uk->plural_expression = '(n % 10 == 1 && n % 100 != 11) ? 0 : ((n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 12 || n % 100 > 14)) ? 1 : 2)';
 		$uk->google_code = 'uk';
 		$uk->facebook_locale = 'uk_UA';
 
