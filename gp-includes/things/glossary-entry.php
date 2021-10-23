30528f3ad19c8094ff3af2f470679e750fab1e9d
Dominik Schilling <dominikschilling+git@gmail.com>
2021-10-23 14:22:28 +0200

    Glossary: Validate part of speech value before inserting into database (#1277)
    * Glossary: Validate part of speech value before inserting into database
    
    * Update changelog

--- gp-includes/things/glossary-entry.php
+++ gp-includes/things/glossary-entry.php
@@ -78,6 +78,7 @@ class GP_Glossary_Entry extends GP_Thing {
 	public function restrict_fields( $rules ) {
 		$rules->term_should_not_be( 'empty' );
 		$rules->part_of_speech_should_not_be( 'empty' );
+		$rules->part_of_speech_should_be( 'one_of', $this->parts_of_speech );
 		$rules->glossary_id_should_be( 'positive_int' );
 		$rules->last_edited_by_should_be( 'positive_int' );
 	}
