3b37e3ca301106153105e2092aa08afa4336ee1b
Dominik Schilling <dominikschilling+git@gmail.com>
2022-03-19 12:41:55 +0100

    Inherit color for warnings discard link, simplify markup for current translation in editor

--- gp-templates/helper-functions.php
+++ gp-templates/helper-functions.php
@@ -280,7 +280,7 @@ function textareas( $entry, $permissions, $index = 0 ) {
 
 		endif;
 		?>
-		<blockquote class="translation"><em><small><?php echo prepare_original( esc_translation( gp_array_get( $entry->translations, $index ) ) ); ?></small></em></blockquote>
+		<blockquote class="translation"><?php echo prepare_original( esc_translation( gp_array_get( $entry->translations, $index ) ) ); ?></blockquote>
 		<textarea class="foreign-text" name="translation[<?php echo esc_attr( $entry->original_id ); ?>][]" id="translation_<?php echo esc_attr( $entry->original_id ); ?>_<?php echo esc_attr( $index ); ?>" <?php echo disabled( ! $can_edit ); ?>><?php echo gp_prepare_translation_textarea( esc_translation( gp_array_get( $entry->translations, $index ) ) ); ?></textarea>
 
 		<div>
