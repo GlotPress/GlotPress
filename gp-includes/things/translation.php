4651038a870c7b7c919a30d80caee95ac95b9344
Dion Hulse <dion@wordpress.org>
2022-03-22 08:37:44 +0000

    Extra whitespace

--- gp-includes/things/translation.php
+++ gp-includes/things/translation.php
@@ -586,12 +586,15 @@ class GP_Translation extends GP_Thing {
 				$member = "translation_$i";
 				unset( $row->$member );
 			}
-			$row->row_id    = $row->original_id . ( $row->id ? "-$row->id" : '' );
+
+			$row->row_id = $row->original_id . ( $row->id ? "-$row->id" : '' );
+
 			if ( isset( $row->priority ) ) {
-				$row->flags     = array(
+				$row->flags = array(
 					'priority=' . GP_Original::$priorities[ $row->priority ],
 				);
 			}
+
 			$translations[] = new Translation_Entry( (array) $row );
 		}
 		unset( $rows );
