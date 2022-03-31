4df1441641ebec8cf8c472e210cee48a12c86507
pedro-mendonca <ped.gaspar@gmail.com>
2022-03-31 14:06:49 +0100

    Add 'ed', 'ion', and 'ing' to suffix list for glossary term ending with 'e'

--- gp-templates/helper-functions.php
+++ gp-templates/helper-functions.php
@@ -108,6 +108,10 @@ function gp_sort_glossary_entries_terms( $glossary_entries ) {
 			$terms[] = substr( $term, 0, -1 ) . 'ves';
 		} elseif ( 'fe' === substr( $term, -2 ) ) {
 			$terms[] = substr( $term, 0, -2 ) . 'ves';
+		} elseif ( 'e' === substr( $term, -1 ) ) {
+			$terms[] = substr( $term, 0, -1 ) . 'ed';
+			$terms[] = substr( $term, 0, -1 ) . 'ion';
+			$terms[] = substr( $term, 0, -1 ) . 'ing';
 		} elseif ( 'an' === substr( $term, -2 ) ) {
 			$terms[] = substr( $term, 0, -2 ) . 'en';
 		} else {
