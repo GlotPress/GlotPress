a2b4e0dd9f6dfa3005c1e13747f0a130f56302b0
pedro-mendonca <ped.gaspar@gmail.com>
2022-03-31 13:48:42 +0100

    Add missing gettext

--- gp-includes/things/glossary.php
+++ gp-includes/things/glossary.php
@@ -203,7 +203,7 @@ class GP_Glossary extends GP_Thing {
 		return new GP::$project(
 			array(
 				'id'   => 0,
-				'name' => 'Locale Glossary',
+				'name' => __( 'Locale Glossary', 'glotpress' ),
 				'slug' => 0,
 				'path' => $locale_glossary_path_prefix,
 			)
