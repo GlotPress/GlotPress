1f8a7d6c7d2e6fe24ed9d6eae121eb5c2498d1a1
Alex Kirk <akirk@users.noreply.github.com>
2022-04-06 16:57:13 +0200

    typo

--- gp-templates/translation-row.php
+++ gp-templates/translation-row.php
@@ -10,7 +10,7 @@ $user            = wp_get_current_user();
 $can_reject_self = ( isset( $translation->user->user_login ) && $user->user_login === $translation->user->user_login && 'waiting' === $translation->translation_status );
 
 if ( is_object( $glossary ) ) {
-	$translation = map_glossary_entries_to_translation_originals( $translation, $glossary);
+	$translation = map_glossary_entries_to_translation_originals( $translation, $glossary );
 }
 
 $translation_singular = isset( $translation->singular_glossary_markup ) ? $translation->singular_glossary_markup : esc_translation( $translation->singular );
