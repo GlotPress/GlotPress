759df3335fbaad4cee666d5b6aeaa83a38c93157
pedro-mendonca <ped.gaspar@gmail.com>
2022-04-20 10:48:33 +0100

    Change navbar home h1 to nav element and make hover consistent

--- gp-templates/header.php
+++ gp-templates/header.php
@@ -16,7 +16,7 @@
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
 
 	<header class="gp-bar clearfix">
-		<h1>
+		<nav id="home-navigation">
 			<a href="<?php echo esc_url( gp_url( '/' ) ); ?>" rel="home">
 				<?php
 				/**
@@ -30,7 +30,7 @@
 				echo apply_filters( 'gp_home_title', 'GlotPress' );
 				?>
 			</a>
-		</h1>
+		</nav>
 
 		<nav id="main-navigation" role="navigation">
 			<?php echo gp_nav_menu(); ?>
