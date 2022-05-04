0ed23eeb5a6a3f3964e4d5dd48d8db9c858e3db4
pedro-mendonca <ped.gaspar@gmail.com>
2022-05-05 00:18:50 +0100

    Update setting label

--- gp-templates/settings-edit.php
+++ gp-templates/settings-edit.php
@@ -60,7 +60,7 @@ if ( empty( $gp_timezone ) ) {
 		</td>
 	</tr>
 	<tr>
-		<th><label for="timezone"><?php _e( 'User Timezone:', 'glotpress' ); ?></label></th>
+		<th><label for="timezone"><?php _e( 'Timezone:', 'glotpress' ); ?></label></th>
 		<td>
 			<select id="timezone" name="timezone" aria-describedby="timezone-description">
 				<?php echo wp_timezone_choice( $gp_timezone, get_user_locale() ); ?>
