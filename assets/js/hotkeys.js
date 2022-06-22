690403489ade33a4c8aec6f7e8fa8cf52223231f
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2022-06-22 12:59:05 +0200

    Add the table for the editor hotkeys

--- assets/js/hotkeys.js
+++ assets/js/hotkeys.js
@@ -11,7 +11,7 @@ jQuery( document ).ready( function( $ ) {
 	$( 'html' ).keydown( function( e ) {
 		var previousPage, nextPage, firstEditorRow;
 		if ( 191 === e.keyCode ) { // Question mark (?) = Show the help with all the hotkeys.
-			$( '#keyboard-shortcuts-container' ).css( 'display', 'block' );
+			$( '#keyboard-shortcuts-container' ).css( 'display', 'flex' );
 			return false;
 		}
 		if ( 27 === e.keyCode ) { // Escape = Hide the help with all the hotkeys.
@@ -53,9 +53,82 @@ jQuery( document ).ready( function( $ ) {
 		'<tr><th colspan="2">Global hotkeys</th><tr>' +
 		'<tr><td><strong>Keyboard shorcut</strong></td><td><strong>Description</strong></td></tr>' +
 		'<tr><td><kbd>?</kbd></td><td>Show this help</td></tr>' +
+		'' +
 		'<tr><td><kbd>Esc</kbd></td><td>Hide this help</td></tr>' +
-		'<tr><td><kbd>Alt</kbd>+<kbd>Left Arrow</kbd> (Windows/Linux)<br><kbd>Option</kbd>+<kbd>Left Arrow</kbd> (macOS)</td><td>Move to the previous page</td></tr>' +
-		'<tr><td><kbd>Alt</kbd>+<kbd>Right Arrow</kbd> (Windows/Linux)<br><kbd>Option</kbd>+<kbd>Right Arrow</kbd> (macOS)</td><td>Move to the next page</td></tr>' +
+		'' +
+		'<tr><td><kbd>Alt</kbd>+<kbd>Left Arrow</kbd> (Windows/Linux)<br>' +
+		'<kbd>Option</kbd>+<kbd>Left Arrow</kbd> (macOS)</td>' +
+		'<td>Move to the previous page</td></tr>' +
+		'' +
+		'<tr><td><kbd>Alt</kbd>+<kbd>Right Arrow</kbd> (Windows/Linux)<br>' +
+		'<kbd>Option</kbd>+<kbd>Right Arrow</kbd> (macOS)</td>' +
+		'<td>Move to the next page</td></tr>' +
+		'</table>' +
+		'' +
+		'<table id="keyboard-shortcuts-table-editor-hotkeys" class="keyboard-shortcuts-table-editor-hotkeys">' +
+		'<tr><th colspan="2">Editor hotkeys</th><tr>' +
+		'<tr><td><kbd>Esc</kbd><br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>Z</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>Z</kbd> (macOS)</td>' +
+		'<td>Cancel</td></tr>' +
+		'' +
+		'<tr><td><kbd>Page Up</kbd><br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Up Arrow</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Up Arrow</kbd> (macOS)</td>' +
+		'<td>Move to the previous editor</td></tr>' +
+		'' +
+		'<tr><td><kbd>Page Down</kbd><br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Down Arrow</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Down Arrow</kbd> (macOS)</td>' +
+		'<td>Move to the next editor</td></tr>' +
+		'' +
+		'<tr><td><kbd>Shift</kbd>+<kbd>Enter</kbd><br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Enter</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Enter</kbd> (macOS)</td>' +
+		'<td>Save or suggest the translation</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>B</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>B</kbd> (macOS)<br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>C</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>C</kbd> (macOS)<br></td>' +
+		'<td>Copy from the original</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>+</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>+</kbd> (macOS)<br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>A</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>A</kbd> (macOS)<br></td>' +
+		'<td>Approve the translation</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>-</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>-</kbd> (macOS)<br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>R</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>R</kbd> (macOS)<br></td>' +
+		'<td>Reject the translation</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>~</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>~</kbd> (macOS)<br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>~</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>~</kbd> (macOS)<br>' +
+		'<kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>F</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>F</kbd> (macOS)<br></td>' +
+		'<td>Set the translation as fuzzy</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>D</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>D</kbd> (macOS)<br>' +
+		'<td>Dismiss validation warnings for the current visible editor</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>D</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Shift</kbd>+<kbd>D</kbd> (macOS)<br>' +
+		'<td>Dismiss validation warnings for the current page</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>Left Arrow</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Left Arrow</kbd> (macOS)<br>' +
+		'<td>Move to the previous page</td></tr>' +
+		'' +
+		'<tr><td><kbd>Ctrl</kbd>+<kbd>Right Arrow</kbd> (Windows/Linux)<br>' +
+		'<kbd>Cmd</kbd>+<kbd>Right Arrow</kbd> (macOS)<br>' +
+		'<td>Move to the next page</td></tr>' +
+		'' +
 		'</table>' +
 		'</div>' +
 		'</div>' +
