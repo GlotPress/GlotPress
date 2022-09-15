4b7ddac9a7f2ac5b5437bb70dc204747dec19f93
Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>
2022-09-15 14:47:05 +0200

    Add a new status: changes requested (#1451)
    * PoC: Add the changes requested status
    
    * Add a new hided button for the new status
    
    * Add a new listener for the new status button
    
    * Add the new status (changes_requested) in the bulk post and bulk approve
    
    * Update a failing test
    
    * Update the package-lock.json with "npm update"
    
    * Update the package-lock.json with "npm update"
    
    * Update the package-lock.json from https://github.com/GlotPress/GlotPress/pull/1452
    
    * Add the minimized JavaScript
    
    * Add some checks to view the new status only with the WPORG_TRANSLATE_BLOGID constant available
    
    To view the new status in the frontend, you need to add the WPORG_TRANSLATE_BLOGID
    constant in the wp-config.php file with the value of the get_current_blog_id() function. E.g.:
    
       define( 'WPORG_TRANSLATE_BLOGID', 1 );
    
    * Update the check to see if the gp-translation-helpers plugin is installed
    
    Check if the 'gp_translation_helper_installed' filter returns true -> plugin installed.
    
    * Update the filter name from gp_translation_helper_installed to gp_enable_changes_requested_status
    
    * Some updates to show the changes_requested strings as rejected if the gp_enable_changes_requested_status is not enabled
    
    * Set the changes_requested as untranslated if the gp_enable_changes_requested_status is not enabled
    
    * Update the margin for the action buttons
    
    * Update the hook for the changes_requested buttons
    
    * Update texts: changes_requested to changesrequested and request_changes to requestchanges
    
    * Resolve a bug in the filter form
    
    * Update the changesrequested showed value, with the uppercase
    
    * Bugfix. Update the value for the changesrequested value if this status is not enabled
    
    * Rearrange some conditions in an if statement
    
    * Add some comments in an if and remove some parenthesis
    
    * Add some comments in an if and remove some parenthesis
    
    * Add a stub for set_status_changesrequested, avoiding breaking the plugin when the gp-translation-helpers plugin is not installed.
    
    Props @akirk https://github.com/GlotPress/GlotPress/pull/1451#pullrequestreview-1091961010

--- gp-templates/translation-row-editor-meta-status.php
+++ gp-templates/translation-row-editor-meta-status.php
@@ -11,20 +11,30 @@
 	<dt><?php _e( 'Status:', 'glotpress' ); ?></dt>
 	<dd>
 		<?php echo display_status( $translation->translation_status ); ?>
-		<?php if ( $translation->translation_status ) : ?>
-			<?php if ( $can_approve_translation ) : ?>
-				<?php if ( 'current' !== $translation->translation_status ) : ?>
-					<button class="button is-small approve" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-current_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Approve this translation. Any existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>+</strong> <span><?php _ex( 'Approve', 'Action', 'glotpress' ); ?></span></button>
-				<?php endif; ?>
-				<?php if ( 'rejected' !== $translation->translation_status ) : ?>
-					<button class="button is-small reject" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-rejected_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Reject this translation. The existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>&minus;</strong> <?php _ex( 'Reject', 'Action', 'glotpress' ); ?></button>
-				<?php endif; ?>
-				<?php if ( 'fuzzy' !== $translation->translation_status ) : ?>
+
+		<?php
+		// Don't show the buttons if the translation status is changesrequested but the changesrequested is not enabled,
+		// because in this situation we consider the changesrequested as rejected translations.
+		if ( 'changesrequested' != $translation->translation_status || apply_filters( 'gp_enable_changesrequested_status', false ) ) : // todo: delete when we merge the gp-translation-helpers in GlotPress.
+		?>
+			<?php if ( $translation->translation_status ) : ?>
+				<?php if ( $can_approve_translation ) : ?>
+					<?php if ( 'current' !== $translation->translation_status ) : ?>
+						<button class="button is-small approve" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-current_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Approve this translation. Any existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>+</strong> <span><?php _ex( 'Approve', 'Action', 'glotpress' ); ?></span></button>
+					<?php endif; ?>
+					<?php if ( ( 'rejected' !== $translation->translation_status ) && ( 'changesrequested' !== $translation->translation_status ) ) : ?>
+						<button class="button is-small reject" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-rejected_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Reject this translation. The existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>&minus;</strong> <?php _ex( 'Reject', 'Action', 'glotpress' ); ?></button>
+						<?php if ( apply_filters( 'gp_enable_changesrequested_status', false ) ) : // todo: delete when we merge the gp-translation-helpers in GlotPress ?>
+							<button class="button is-small changesrequested" style="display: none;" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-changesrequested_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Request changes for this translation. The existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>&minus;</strong> <?php _ex( 'Request changes', 'Action', 'glotpress' ); ?></button>
+						<?php endif; ?>
+					<?php endif; ?>
+					<?php if ( 'fuzzy' !== $translation->translation_status ) : ?>
+						<button class="button is-small fuzzy" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-fuzzy_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Mark this translation as fuzzy for further review.', 'glotpress' ); ?>"><strong>~</strong> <span><?php _ex( 'Fuzzy', 'Action', 'glotpress' ); ?></span></button>
+					<?php endif; ?>
+				<?php elseif ( $can_reject_self ) : ?>
+					<button class="button is-small reject" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-rejected_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Reject this translation. The existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>&minus;</strong> <span><?php _ex( 'Reject', 'Action', 'glotpress' ); ?></span></button>
 					<button class="button is-small fuzzy" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-fuzzy_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Mark this translation as fuzzy for further review.', 'glotpress' ); ?>"><strong>~</strong> <span><?php _ex( 'Fuzzy', 'Action', 'glotpress' ); ?></span></button>
 				<?php endif; ?>
-			<?php elseif ( $can_reject_self ) : ?>
-				<button class="button is-small reject" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-rejected_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Reject this translation. The existing translation will be kept as part of the translation history.', 'glotpress' ); ?>"><strong>&minus;</strong> <span><?php _ex( 'Reject', 'Action', 'glotpress' ); ?></span></button>
-				<button class="button is-small fuzzy" data-nonce="<?php echo esc_attr( wp_create_nonce( 'update-translation-status-fuzzy_' . $translation->id ) ); ?>" title="<?php esc_attr_e( 'Mark this translation as fuzzy for further review.', 'glotpress' ); ?>"><strong>~</strong> <span><?php _ex( 'Fuzzy', 'Action', 'glotpress' ); ?></span></button>
 			<?php endif; ?>
 		<?php endif; ?>
 	</dd>
