e722b12e2e76727f1d98a689e1b9cb2cd56427fb
Alex Kirk <akirk@users.noreply.github.com>
2023-02-22 16:28:54 +0100

    Local inline translation (#1573)
    * Add the backend with settings and a list with local items (core, plugins, themes)
    
    * Save the checkbox value to enable local translations
    
    * Add a message to hide the local translations if you have not enable them
    
    * Add the originals and the translations strings for the core
    
    * Split the translate_core method in smaller methods
    
    * Add the 4 WordPress core subprojects and the main project.
    
    * Add the functionality to translate locally a plugin
    
    * Add the functionality to translate locally a theme
    
    * Redirect to the selected plugin or theme, not to the main project
    
    * Add the nonces in the URL and their checks
    
    * Add some checks
    
    * Download the .po and .mo files from translate.w.org if they don't exist locally
    
    * Redirect to the locale in the plugins and themes
    
    * Check if the file downloaded from DotOrg has fired an error
    
    * Add the local "languages" folder inside the plugins and themes to look for .po files
    
    * Initial import of community-translator
    
    * Preload translations
    
    * Make saving work
    
    * Fix lint errors
    
    * JS Lint fixes
    
    * Fix eslint errors
    
    * Update for new eslint settings
    
    * Incorporate text domain namespacing more deeply
    
    * Lint fixes
    
    * Lint fix
    
    * fix warning
    
    * Fix wrong parameter sequence
    
    * typo
    
    * Fix original lookup
    
    * Speedup initial tree walking
    
    * Looku placeholders
    
    * Show user translations
    
    * Modify translation after saving
    
    * Fix linting
    
    * Prioritize found translations when the context is dubious
    
    * Remove help icon for now
    
    * Expose the text domain for translations that were not found
    
    * Don't replace placeholders as if they were new translations
    
    ---------
    
    Co-authored-by: Jesús Amieiro <1667814+amieiro@users.noreply.github.com>

--- gp-settings.php
+++ gp-settings.php
@@ -127,6 +127,11 @@ require_once GP_PATH . GP_INC . 'formats/format-jed1x.php';
 require_once GP_PATH . GP_INC . 'formats/format-ngx.php';
 require_once GP_PATH . GP_INC . 'formats/format-php.php';
 
+if ( GP_Local::is_active() ) {
+	require_once GP_PATH . GP_INC . 'inline-translation.php';
+	GP_Inline_Translation::init();
+}
+
 // Let's do it again, there are more variables added since last time we called it.
 gp_set_globals( get_defined_vars() );
 
