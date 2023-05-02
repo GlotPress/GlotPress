6c0318a9d7bc40a16b82a4ac977dde182e78ac59
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-05-02 12:27:35 +0200

    Add 3 files to the conditional loading: 2 CSS and 1 JS

--- gp-includes/inline-translation.php
+++ gp-includes/inline-translation.php
@@ -129,12 +129,14 @@ class GP_Inline_Translation {
 	 * Enqueue the scripts and styles for the translator.
 	 */
 	public function enqueue_scripts() {
+		$css_extension = SCRIPT_DEBUG || GP_SCRIPT_DEBUG ? '.css' : '.min.css';
+		$js_extension = SCRIPT_DEBUG || GP_SCRIPT_DEBUG ? '.js' : '.min.js';
 		$inline_translation_js_file = SCRIPT_DEBUG || GP_SCRIPT_DEBUG ? 'inline-translation/inline-translation.js' : 'assets/js/inline-translation.min.js';
 
-		wp_enqueue_style( 'inline-translation-loader', gp_plugin_url( 'assets/css/inline-translation-loader.css', __FILE__ ) ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
-		wp_enqueue_style( 'inline-translation', gp_plugin_url( 'assets/css/inline-translation.css', __FILE__ ) ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
-		wp_enqueue_script( 'inline-translation', gp_plugin_url( $inline_translation_js_file ), array( 'jquery' ), false, true ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.NoExplicitVersion
-		wp_enqueue_script( 'inline-translation-loader', gp_plugin_url( 'assets/js/inline-translation-loader.js', __FILE__ ), array( 'inline-translation' ), false, true ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.NoExplicitVersion
+		wp_enqueue_style( 'inline-translation-loader', gp_plugin_url( 'assets/css/inline-translation-loader' . $css_extension, __FILE__ ) ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+		wp_enqueue_style( 'inline-translation', gp_plugin_url( 'assets/css/inline-translation' . $css_extension, __FILE__ ) ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
+		wp_enqueue_script( 'inline-translation', gp_plugin_url( $inline_translation_js_file ), array( 'jquery', 'jquery-ui-tooltip' ), false, true ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.NoExplicitVersion
+		wp_enqueue_script( 'inline-translation-loader', gp_plugin_url( 'assets/js/inline-translation-loader' . $js_extension, __FILE__ ), array( 'inline-translation' ), false, true ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.NoExplicitVersion
 	}
 
 	/**
