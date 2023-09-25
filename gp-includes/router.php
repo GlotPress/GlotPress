2314ff2f0ab844c42a7adaf59a49a6ac78a30d96
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-09-25 17:09:13 +0200

    Remove the check of the last character in $_SERVER['REQUEST_URI']

--- gp-includes/router.php
+++ gp-includes/router.php
@@ -181,10 +181,6 @@ class GP_Router {
 
 		$url_path = gp_url_path( gp_url_public_root() );
 
-		// Check if the last character of the text is not a slash, and add it if needed
-		if ( substr( $_SERVER['REQUEST_URI'], -1 ) !== '/' ) {
-			$_SERVER['REQUEST_URI'] .= '/';
-		}
 		// If the request URL doesn't match our base URL, don't bother trying to match
 		if ( $url_path && ! gp_startswith( wp_unslash( trailingslashit( $_SERVER['REQUEST_URI'] ) ), $url_path ) ) {
 			return;
