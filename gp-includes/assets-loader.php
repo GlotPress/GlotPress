76a8b0242f93c40e39a4f4d18225ab687b396d52
Tosin Oguntuyi <tcubed03@gmail.com>
2023-10-05 18:04:09 +0100

    Register locally added driver-js css and js files

--- gp-includes/assets-loader.php
+++ gp-includes/assets-loader.php
@@ -17,7 +17,7 @@ function gp_register_default_styles() {
 	// Register our base style.
 	wp_register_style( 'gp-base', $url . '/style' . $suffix, array(), filemtime( $path . 'style' . $suffix ) );
 	wp_register_style( 'gp-jquery-webui-popover', $url . '/jquery.webui-popover' . $suffix, array( 'gp-base' ), filemtime( $path . 'jquery.webui-popover' . $suffix ) );
-	wp_register_style( 'driver-js', 'https://cdn.jsdelivr.net/npm/driver.js@1.0.1/dist/driver.css', array(), '1.0.1' );
+	wp_register_style( 'driver-js',  $url . '/driver-js' . $suffix, array(), '1.3.0' );
 }
 
 add_action( 'init', 'gp_register_default_styles' );
@@ -37,15 +37,16 @@ function gp_register_default_scripts() {
 	wp_register_script( 'gp-glossary', $url . '/glossary' . $suffix, array( 'gp-editor' ), filemtime( $path . 'glossary' . $suffix ) );
 	wp_register_script( 'gp-translations-page', $url . '/translations-page' . $suffix, array( 'gp-editor' ), filemtime( $path . 'translations-page' . $suffix ) );
 	wp_register_script( 'gp-mass-create-sets-page', $url . '/mass-create-sets-page' . $suffix, array( 'gp-editor' ), filemtime( $path . 'mass-create-sets-page' . $suffix ) );
+	wp_register_script( 'gp-jquery-webui-popover', $url . '/jquery.webui-popover' . $suffix, array( 'jquery' ), filemtime( $path . 'jquery.webui-popover' . $suffix ), false );
 	wp_register_script( 'gp-tour', $url . '/tour' . $suffix, array( 'jquery', 'gp-jquery-webui-popover' ), filemtime( $path . 'tour' . $suffix ), false );
-	wp_register_script( 'driver-js', 'https://cdn.jsdelivr.net/npm/driver.js@1.0.1/dist/driver.js.iife.js', array(), '1.0.1' );
+	wp_register_script( 'driver-js', $url . '/driver-js' . $suffix, array(), '1.3.0' );
 
 	wp_set_script_translations( 'gp-common', 'glotpress' );
 	wp_set_script_translations( 'gp-editor', 'glotpress' );
 	wp_set_script_translations( 'gp-glossary', 'glotpress' );
 	wp_set_script_translations( 'gp-mass-create-sets-page', 'glotpress' );
 	wp_set_script_translations( 'gp-tour', 'glotpress' );
-	wp_register_script( 'gp-jquery-webui-popover', $url . '/jquery.webui-popover' . $suffix, array( 'jquery' ), filemtime( $path . 'jquery.webui-popover' . $suffix ), false );
+
 	wp_localize_script( 'gp-tour', 'gp_tour', apply_filters( 'gp_tour', array() ) );
 }
 
