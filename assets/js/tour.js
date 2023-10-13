89de5bede24fa5aff2247b765f19b4712f094388
Tosin <tcubed03@gmail.com>
2023-10-13 11:54:10 +0100

    Site tour: Use driver.js (#1714)
    * Use driver.js for site tour
    
    * Fix lint errors
    
    * Add driver-js locally
    
    * Register locally added driver-js css and js files
    
    * Ignore driver-js in eslint
    
    * Fix phpcs error
    
    * Patch driver.js to apply popover on visible elements only
    
    * Change hardcoded version to time to bust cache
    
    * Add driver-js as dependency for tour.js
    
    * Add attribution to driver-js
    
    ---------
    
    Co-authored-by: Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -1,83 +1,30 @@
-/* global wp, document, window, WebuiPopovers, gp_tour */
+/* global , document, window, gp_tour */
 /* eslint camelcase: "off" */
 
 jQuery( document ).ready(
 	function() {
-		jQuery( document ).on( 'click', '.close-tour, .dismiss-tour', function() {
-			jQuery( this ).closest( '.webui-popover' ).hide();
-			jQuery( '.pulse-wrapper' ).removeClass( 'pulse-border' );
-			return false;
-		} );
-		jQuery( document ).on( 'click', '.next-tour-item', function() {
-			jQuery( '.pulse-wrapper .tour-' + jQuery( this ).data( 'tourname' ) + ':visible' ).first().click();
-			return false;
-		} );
-		jQuery( document ).on( 'click', '.previous-tour-item', function() {
-			var currentPopover = jQuery( this ).closest( '.webui-popover' );
-			if ( currentPopover.prev().hasClass( 'webui-popover' ) ) {
-				currentPopover.hide();
-				currentPopover.prev().show();
-			}
-			return false;
-		} );
-		jQuery( document ).on( 'click', '.reveal-next-tour-item', function() {
-			jQuery( this ).closest( '.webui-popover' ).hide();
-			jQuery( jQuery( this ).data( 'reveal' ) ).first().click();
-			return false;
-		} );
-
 		jQuery( document ).on( 'click', '.pulse', function() {
+			var driver = window.driver.js.driver;
 			var wrapper = jQuery( this ).closest( '.pulse-wrapper' );
 			var tourName = wrapper.data( 'tourname' );
-			var currentTourIndex = wrapper.data( 'tourindex' );
-			var nextItem = 1 + currentTourIndex;
-			var tourEndsHere = typeof window.tour[ tourName ][ nextItem ] === 'undefined';
-			var showPreviousBtn = currentTourIndex > 1;
-			var popoverContent = wrapper.data( 'popover-content' );
-			var item;
-			var istopOffsetDefined = typeof window.tour[ tourName ][ currentTourIndex ].topOffset !== 'undefined' && Number.isInteger( window.tour[ tourName ][ currentTourIndex ].topOffset );
-			var popoverTopOffset = istopOffsetDefined ? window.tour[ tourName ][ currentTourIndex ].topOffset : 0;
-			var isPlacementDefined = typeof window.tour[ tourName ][ currentTourIndex ].placement !== 'undefined';
-			var popoverPlacement = isPlacementDefined ? window.tour[ tourName ][ currentTourIndex ].placement : 'bottom-right';
-			if ( ! tourEndsHere ) {
-				// Check if the selector for the next item does not exists
-				if ( jQuery( window.tour[ tourName ][ nextItem ].selector + ':visible' ).length < 1 ) {
-					while ( nextItem < window.tour[ tourName ].length ) {
-						if ( jQuery( window.tour[ tourName ][ nextItem ].selector + ':visible' ).length > 0 ) {
-							break;
-						}
-						nextItem++;
-					}
+			var tourTitle = window.tour[ tourName ][ 0 ].title;
+			var tourSteps = jQuery.map( window.tour[ tourName ], function( item ) {
+				if ( typeof item.title === 'undefined' ) {
+					return {
+						element: item.selector,
+						popover: {
+							title: tourTitle,
+							description: item.html,
+						},
+					};
 				}
-			}
+			} );
 
-			item = window.tour[ tourName ][ nextItem ];
-
-			jQuery( '.pulse-wrapper' ).removeClass( 'pulse-border' );
-			jQuery( '.webui-popover:visible' ).hide();
-			wrapper.addClass( 'pulse-border' );
-
-			if ( tourEndsHere ) {
-				popoverContent += '<br/><br/><a href="" class="close-tour">' + wp.i18n.__( 'Close', 'glotpress' ) + '</a>';
-			} else if ( typeof window.tour[ tourName ][ nextItem ] !== 'undefined' && typeof window.tour[ tourName ][ nextItem ].reveal === 'undefined' ) {
-				popoverContent += '<div class="popover-nav-btns">';
-				popoverContent += showPreviousBtn ? '<br/><br/><a href="" class="tour-button previous-tour-item" data-tourname="' + tourName + '">' + wp.i18n.__( 'Previous', 'glotpress' ) + '</a>' : '';
-				popoverContent += '<a href="" class="tour-button next-tour-item button-primary" data-tourname="' + tourName + '">Next</a>';
-				popoverContent += '</div>';
-			} else if ( typeof window.tour[ tourName ][ nextItem ].reveal !== 'undefined' ) {
-				popoverContent += '<br/><br/><a href="" class="reveal-next-tour-item" data-reveal="' + window.tour[ tourName ][ nextItem ].reveal + '">' + wp.i18n.__( 'Reveal Next Step', 'glotpress' ) + '</a>';
-			}
-			if ( ! tourEndsHere ) {
-				popoverContent += '<br/><small><a href="" class="dismiss-tour">' + wp.i18n.__( 'Dismiss this tour', 'glotpress' );
-			}
-			WebuiPopovers.show( this, { title: window.tour[ tourName ][ 0 ].title, content: popoverContent, width: 300, dismissible: true, offsetTop: popoverTopOffset, placement: popoverPlacement } );
-			jQuery( '.tour-' + tourName ).remove();
-
-			if ( tourEndsHere ) {
-				return;
-			}
-
-			addPulse( jQuery( item.selector ), item, tourName, nextItem );
+			var driverObj = driver( {
+				showProgress: true,
+				steps: tourSteps,
+			} );
+			driverObj.drive();
 		} );
 
 		function addPulse( field, item, tourName, index ) {
