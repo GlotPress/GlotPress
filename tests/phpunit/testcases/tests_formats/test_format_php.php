59e116861391728d66f248428b1b03bdb9a390b1
Pascal Birchler <pascalb@google.com>
2023-12-20 14:44:37 +0100

    Clean up tests as per code review

--- tests/phpunit/testcases/tests_formats/test_format_php.php
+++ tests/phpunit/testcases/tests_formats/test_format_php.php
@@ -55,8 +55,6 @@ class GP_Test_Format_PHP extends GP_UnitTestCase {
 
 		$php = GP::$formats[ $this->format ]->print_exported_file( $this->translation_set->project, $this->locale, $this->translation_set, $entries );
 
-		var_dump($php);
-
 		$this->assertStringContainsString("'foo'=>'bar'", $php );
 		$this->assertStringContainsString("'somecontext\4bar'=>'baz'", $php );
 		$this->assertStringContainsString("'%d Theme Update'=>'%d Theme Update' . \"\\0\" . '%d Theme Updates'", $php );
@@ -68,75 +66,5 @@ class GP_Test_Format_PHP extends GP_UnitTestCase {
 
 	public function test_read_translations_from_file() {
 		$this->markTestIncomplete( 'Not implemented' );
-	}
-
-	/**
-	 * Returns the expected data for the parsed example-untranslated.php file.
-	 */
-	public function data_example_originals() {
-		$translations = new Translations();
-		$translations->add_entry( new Translation_Entry( array(
-			'singular' => 'This file is too big. Files must be less than %d KB in size.',
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular' => '%d Theme Update',
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular' => 'Medium',
-			'context'  => 'password strength',
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular' => 'Category',
-			'context'  => 'taxonomy singular name',
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular' => 'Pages',
-			'context'  => 'post type general name',
-		) ) );
-
-		return $translations;
-	}
-
-	/**
-	 * Returns the expected data for the parsed example-untranslated.php file.
-	 */
-	public function data_example_translations() {
-		$translations = new Translations();
-		$translations->add_entry( new Translation_Entry( array(
-			'singular'     => 'This file is too big. Files must be less than %d KB in size.',
-			'translations' => array(
-				'Diese Datei ist zu gross. Dateien müssen kleiner als %d KB sein.',
-			),
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular'     => '%d Theme Update',
-			'translations' => array(
-				'%d Theme-Aktualisierung',
-				'%d Theme-Aktualisierungen',
-			)
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular'     => 'Medium',
-			'context'      => 'password strength',
-			'translations' => array(
-				'Medium',
-			)
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular'     => 'Category',
-			'context'      => 'taxonomy singular name',
-			'translations' => array(
-				'Kategorie',
-			)
-		) ) );
-		$translations->add_entry( new Translation_Entry( array(
-			'singular'     => 'Pages',
-			'context'      => 'post type general name',
-			'translations' => array(
-				'Seiten',
-			)
-		) ) );
-
-		return $translations;
 	}
 }
