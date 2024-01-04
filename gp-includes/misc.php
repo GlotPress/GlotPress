41e444dafcd369904d2755bb6541544ea4c3d3a3
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2024-01-04 16:45:49 +0100

    Improve the checks in the gp_member_get function

--- gp-includes/misc.php
+++ gp-includes/misc.php
@@ -83,7 +83,7 @@ function gp_const_set( $name, $value ) {
 
 
 function gp_member_get( $object, $key, $default = '' ) {
-	return isset( $object->$key ) ? $object->$key : $default;
+	return is_string( $key ) && property_exists( $object, $key ) ? $object->$key : $default;
 }
 
 /**
