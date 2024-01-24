8605f96d6452bed231e2fd7a16a5efeeea4584a0
Paulo Pinto <regularjack@gmail.com>
2024-01-24 16:57:23 +0000

    Rename function
    Co-authored-by: Jesús Amieiro Becerra <1667814+amieiro@users.noreply.github.com>

--- gp-includes/install-upgrade.php
+++ gp-includes/install-upgrade.php
@@ -11,7 +11,7 @@
  *
  * @since 4.0.0
  */
-function gp_db_upgrade_required(): bool {
+function gp_is_db_upgrade_required(): bool {
 	$gp_db_version = get_option( 'gp_db_version' );
 
 	return false === $gp_db_version || GP_DB_VERSION > $gp_db_version;
