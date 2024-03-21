46c0f6387379f2cd82bff08fd0b05d67e5bd33f7
pedro-mendonca <ped.gaspar@gmail.com>
2024-03-21 11:52:35 +0000

    Sanitize gp_get() for new glossary translation_set_id string

--- gp-includes/routes/glossary.php
+++ gp-includes/routes/glossary.php
@@ -16,7 +16,15 @@ class GP_Route_Glossary extends GP_Route_Main {
 
 	public function new_get() {
 		$glossary                     = new GP_Glossary();
-		$glossary->translation_set_id = gp_get( 'translation_set_id' );
+
+		$translation_set_id = gp_get( 'translation_set_id' );
+
+		if ( ! is_string( $translation_set_id ) ) {
+			$this->redirect_with_error( __( 'Couldn&#8217;t find translation set with this ID.', 'glotpress' ) );
+			return;
+		}
+
+		$glossary->translation_set_id = $translation_set_id;
 
 		$translation_set = $glossary->translation_set_id ? GP::$translation_set->get( $glossary->translation_set_id ) : null;
 
