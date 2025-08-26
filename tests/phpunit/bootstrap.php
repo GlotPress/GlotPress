b9b821e432dd94692cb7c580f9e4fa620e9bd2c9
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2025-08-26 16:22:27 +0200

    Add the WP_TESTS_SKIP_DEPRECATION constant

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -6,6 +6,9 @@
  * @subpackage Tests
  */
 
+// Skip WordPress deprecation tracking during tests to avoid cluttering test output with deprecation notices.
+define( 'WP_TESTS_SKIP_DEPRECATION', true );
+
 if ( ! defined( 'GP_TESTS_DIR' ) ) {
 	define( 'GP_TESTS_DIR', __DIR__ );
 }
