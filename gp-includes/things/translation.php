5c43a76a65e269935d13f1408181bd6792731e1f
Peter Smits <peter@psmits.com>
2026-05-17 13:48:40 +0200

    Fixed tabs issues And added the line below in format-pomo to prevent errors // phpcs:ignore Generic.Files.OneObjectStructurePerFile.MultipleFound

--- gp-includes/things/translation.php
+++ gp-includes/things/translation.php
@@ -744,9 +744,7 @@ class GP_Translation extends GP_Thing {
 		if ( $can_set_status && 'current' === $desired_status ) {
 			$original = GP::$original->get( $this->original_id );
 			if ( $original && ! empty( $original->plural ) ) {
-				$locale = GP_Locales::by_slug(
-					GP::$translation_set->get( $this->translation_set_id )->locale
-				);
+				$locale = GP_Locales::by_slug(GP::$translation_set->get( $this->translation_set_id )->locale);
 				foreach ( range( 0, $locale->nplurals - 1 ) as $i ) {
 					if ( empty( $this->{"translation_$i"} ) ) {
 						$can_set_status = false;
